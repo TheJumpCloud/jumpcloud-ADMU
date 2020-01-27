0f20321fc8997242e60061ab4d7a5d9d346d53ef
epanipinto-jc <45767377+epanipinto-jc@users.noreply.github.com>
2020-01-27 14:49:39 -0700

    clean up migration refs

--- powershell/Start-JCADMU.ps1
+++ powershell/Start-JCADMU.ps1
@@ -7,7 +7,7 @@ $formResults = Invoke-Expression -Command:('& "' + $scriptPath + '\Form.ps1"')
 # Send form results to process if $formresults & securechannel true
 If (-not [System.String]::IsNullOrEmpty($formResults))
 {
-    & ($scriptPath + '\Migration.ps1') -inputObject:($formResults)
+    Start-Migration -inputObject:($formResults)
 }
 Else
 {
