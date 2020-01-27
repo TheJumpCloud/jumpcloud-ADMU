3876a93ba9d1f1bafd5e03abea239797ae902576
epanipinto-jc <45767377+epanipinto-jc@users.noreply.github.com>
2020-01-27 14:49:39 -0700

    clean up migration refs

--- powershell/Build.ps1
+++ powershell/Build.ps1
@@ -7,15 +7,12 @@ If (Test-Path -Path:($Output)) { Remove-Item -Path:($Output) }
 $StartJCADMU = (Get-Content -Path:($RootPath + '\Start-JCADMU.ps1') -Raw) -Replace ("`r", "")
 $Functions = (Get-Content -Path:($RootPath + '\Functions.ps1') -Raw) -Replace ("`r", "")
 $Form = (Get-Content -Path:($RootPath + '\Form.ps1') -Raw) -Replace ("`r", "")
-$Migration = (Get-Content -Path:($RootPath + '\Migration.ps1') -Raw) -Replace ("`r", "")
 # String manipulation
 $NewContent = $StartJCADMU
 $NewContent = $NewContent.Replace('# Get script path' + "`n", '')
 $NewContent = $NewContent.Replace('$scriptPath = (Split-Path -Path:($MyInvocation.MyCommand.Path))' + "`n", '')
-$Functions = $Functions + "`n" + $Migration
 $NewContent = $NewContent.Replace('& ($scriptPath + ''\Functions.ps1'')', $Functions)
 $NewContent = $NewContent.Replace('$formResults = Invoke-Expression -Command:(''& "'' + $scriptPath + ''\Form.ps1"'')' + "`n", $Form)
-$NewContent = $NewContent.Replace('& ($scriptPath + ''\Migration.ps1'') -inputObject:($formResults)', 'Start-Migration -inputObject:($formResults)')
 $NewContent = $NewContent.Replace('Return $FormResults' + "`n" + '}', '')
 $NewContent = $NewContent + "`n" + '}'
 # Export combined file
