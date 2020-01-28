3e884111e694382753b1864cd1e1bb75f84fb8cd
btstevens <brad.stevens@gmail.com>
2020-01-28 12:05:01 -0700

    formating

--- powershell/Tests/Build.Tests.ps1
+++ powershell/Tests/Build.Tests.ps1
@@ -2,13 +2,10 @@ $psfolder = Split-Path -Path:($MyInvocation)
 Write-Output $psfolder
 Describe 'Build Tests' {
 
-    Context 'Check Files Exist'{
+    Context 'Check Files Exist' {
 
-       It 'ps2exe.ps1 exists' {
-         (Test-Path -Path 'C:\tools\PS2EXE-GUI\ps2exe.ps1') | Should Be $true
+        It 'ps2exe.ps1 exists' {
+            (Test-Path -Path 'C:\tools\PS2EXE-GUI\ps2exe.ps1') | Should Be $true
         }
-
-
     }
-
 }
