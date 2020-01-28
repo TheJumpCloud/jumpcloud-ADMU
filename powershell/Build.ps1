9a21f677a4f19fa6df61c83483abd0a1a6ff4822
btstevens <brad.stevens@gmail.com>
2020-01-28 11:27:12 -0700

    add_trim

--- powershell/Build.ps1
+++ powershell/Build.ps1
@@ -8,11 +8,11 @@ $StartJCADMU = (Get-Content -Path:($RootPath + '\Start-JCADMU.ps1') -Raw) -Repla
 $Functions = (Get-Content -Path:($RootPath + '\Functions.ps1') -Raw) -Replace ("`r", "")
 $Form = (Get-Content -Path:($RootPath + '\Form.ps1') -Raw) -Replace ("`r", "")
 # String manipulation
-$NewContent = $StartJCADMU
+$NewContent = $StartJCADMU.Trim()
 $NewContent = $NewContent.Replace('# Get script path' + "`n", '')
 $NewContent = $NewContent.Replace('$scriptPath = (Split-Path -Path:($MyInvocation.MyCommand.Path))' + "`n", '')
-$NewContent = $NewContent.Replace('& ($scriptPath + ''\Functions.ps1'')', $Functions)
-$NewContent = $NewContent.Replace('$formResults = Invoke-Expression -Command:(''& "'' + $scriptPath + ''\Form.ps1"'')' + "`n", $Form)
+$NewContent = $NewContent.Replace('& ($scriptPath + ''\Functions.ps1'')', $Functions.Trim())
+$NewContent = $NewContent.Replace('$formResults = Invoke-Expression -Command:(''& "'' + $scriptPath + ''\Form.ps1"'')' + "`n", $Form.Trim())
 $NewContent = $NewContent.Replace('Return $FormResults' + "`n" + '}', '')
 $NewContent = $NewContent + "`n" + '}'
 # Export combined file
