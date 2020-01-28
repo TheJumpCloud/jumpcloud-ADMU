246aa603506fcc221284a314437b92faf4b8281b
btstevens <brad.stevens@gmail.com>
2020-01-28 08:18:51 -0700

    Previous_agent_installer

--- powershell/Functions.ps1
+++ powershell/Functions.ps1
@@ -338,7 +338,7 @@ Function AgentIsOnFileSystem()
 }
 Function InstallAgent()
 {
-    $params = ("${AGENT_INSTALLER_PATH}", "-k ${JumpCloudConnectKey}", "/VERYSILENT", "/NORESTART", "/SUPRESSMSGBOXES", "/NOCLOSEAPPLICATIONS", "/NORESTARTAPPLICATIONS", "/LOG=$env:TEMP\jcUpdate.log")
+    $params = ("${OLD_AGENT_INSTALLER_PATH}", "-k ${JumpCloudConnectKey}", "/VERYSILENT", "/NORESTART", "/SUPRESSMSGBOXES", "/NOCLOSEAPPLICATIONS", "/NORESTARTAPPLICATIONS", "/LOG=$env:TEMP\jcUpdate.log")
     Invoke-Expression "$params"
   }
 
