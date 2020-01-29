2e5e0a1750b73c26b9d33bc068cebe837d3fa035
epanipinto-jc <45767377+epanipinto-jc@users.noreply.github.com>
2020-01-29 14:57:30 -0700

    remove function wrapper

--- powershell/Invoke-GitCommit.ps1
+++ powershell/Invoke-GitCommit.ps1
@@ -1,36 +1,33 @@
-Function Invoke-GitCommit
+param (
+    [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true, Position = 0)][ValidateNotNullOrEmpty()][string]$BranchName
+)
+# Logging
+$MyName = $MyInvocation.MyCommand.Name
+$ParentScriptName = (Get-PSCallStack | Where-Object { $_.Command -notin ($MyName, $MyName.Replace('.ps1', '')) }).Command -join ','
+$CommitMessage = 'Push to ' + $BranchName + '; Called by:' + $ParentScriptName + ';[skip ci]'
+$UserEmail = 'AzurePipelines@FakeEmail.com'
+$UserName = 'AzurePipelines'
+Function Invoke-Git
 {
-    param (
-        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true, Position = 0)][ValidateNotNullOrEmpty()][string]$BranchName
-    )
-    # Logging
-    $MyName = $MyInvocation.MyCommand.Name
-    $ParentScriptName = (Get-PSCallStack | Where-Object { $_.Command -notin ($MyName, $MyName.Replace('.ps1', '')) }).Command -join ','
-    $CommitMessage = 'Push to ' + $BranchName + '; Called by:' + $ParentScriptName + ';[skip ci]'
-    $UserEmail = 'AzurePipelines@FakeEmail.com'
-    $UserName = 'AzurePipelines'
-    Function Invoke-Git
+    Param($Arguments)
+    If ([environment]::OSVersion.Platform -eq 'Win32NT') { $env:GIT_REDIRECT_STDERR = '2>&1' }
+    $LASTEXITCODE = 0
+    $Error.Clear()
+    $Command = 'git ' + $Arguments
+    Write-Host ('[GitCommand]' + $Command)
+    Invoke-Expression -Command:($Command)
+    If ($LASTEXITCODE)
     {
-        Param($Arguments)
-        If ([environment]::OSVersion.Platform -eq 'Win32NT') { $env:GIT_REDIRECT_STDERR = '2>&1' }
-        $LASTEXITCODE = 0
-        $Error.Clear()
-        $Command = 'git ' + $Arguments
-        Write-Host ('[GitCommand]' + $Command)
-        Invoke-Expression -Command:($Command)
-        If ($LASTEXITCODE)
-        {
-            Throw ('Git error, $LASTEXITCODE: ' + $LASTEXITCODE)
-        }
-        If ($Error)
-        {
-            Throw ('Git error, $Error: ' + $Error)
-        }
+        Throw ('Git error, $LASTEXITCODE: ' + $LASTEXITCODE)
     }
-    Invoke-Git -Arguments:('config user.email "' + $UserEmail + '";')
-    Invoke-Git -Arguments:('config user.name "' + $UserName + '";')
-    Invoke-Git -Arguments:('add -A;')
-    Invoke-Git -Arguments:('status;')
-    Invoke-Git -Arguments:('commit -m ' + '"' + $CommitMessage + '";')
-    Invoke-Git -Arguments:('push origin HEAD:refs/heads/' + $BranchName + ';')
+    If ($Error)
+    {
+        Throw ('Git error, $Error: ' + $Error)
+    }
 }
+Invoke-Git -Arguments:('config user.email "' + $UserEmail + '";')
+Invoke-Git -Arguments:('config user.name "' + $UserName + '";')
+Invoke-Git -Arguments:('add -A;')
+Invoke-Git -Arguments:('status;')
+Invoke-Git -Arguments:('commit -m ' + '"' + $CommitMessage + '";')
+Invoke-Git -Arguments:('push origin HEAD:refs/heads/' + $BranchName + ';')
