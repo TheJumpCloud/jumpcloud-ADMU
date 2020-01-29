98f91b329a595467cc9a3db44e094a6e8626d19e
epanipinto-jc <45767377+epanipinto-jc@users.noreply.github.com>
2020-01-29 14:55:59 -0700

    added missing function

--- powershell/Invoke-GitCommit.ps1
+++ powershell/Invoke-GitCommit.ps1
@@ -9,6 +9,24 @@ Function Invoke-GitCommit
     $CommitMessage = 'Push to ' + $BranchName + '; Called by:' + $ParentScriptName + ';[skip ci]'
     $UserEmail = 'AzurePipelines@FakeEmail.com'
     $UserName = 'AzurePipelines'
+    Function Invoke-Git
+    {
+        Param($Arguments)
+        If ([environment]::OSVersion.Platform -eq 'Win32NT') { $env:GIT_REDIRECT_STDERR = '2>&1' }
+        $LASTEXITCODE = 0
+        $Error.Clear()
+        $Command = 'git ' + $Arguments
+        Write-Host ('[GitCommand]' + $Command)
+        Invoke-Expression -Command:($Command)
+        If ($LASTEXITCODE)
+        {
+            Throw ('Git error, $LASTEXITCODE: ' + $LASTEXITCODE)
+        }
+        If ($Error)
+        {
+            Throw ('Git error, $Error: ' + $Error)
+        }
+    }
     Invoke-Git -Arguments:('config user.email "' + $UserEmail + '";')
     Invoke-Git -Arguments:('config user.name "' + $UserName + '";')
     Invoke-Git -Arguments:('add -A;')
