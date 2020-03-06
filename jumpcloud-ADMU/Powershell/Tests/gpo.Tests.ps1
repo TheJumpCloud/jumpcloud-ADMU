aa667b2ede7107bf4d7ab856116100125b50148f
btstevens <brad.stevens@gmail.com>
2020-03-06 12:41:25 -0700

    fix manifest_dir

--- jumpcloud-ADMU/Powershell/Tests/gpo.Tests.ps1
+++ jumpcloud-ADMU/Powershell/Tests/gpo.Tests.ps1
@@ -3,7 +3,7 @@ Describe 'GPO Folder Checks' {
     Context 'Check Files & Folders Exist'{
 
        It 'manifest.xml exists' {
-           Test-Path -Path 'C:\agent\_work\1\s\gpo\manifest.xml' | Should Be $true
+           (Test-Path -Path ($Env:BUILD_SOURCESDIRECTORY + '\jumpcloud-ADMU\Gpo\manifest.xml')) | Should Be $true
        }
 
     }
