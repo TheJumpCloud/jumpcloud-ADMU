7c838cf4d9871a6a08d5b58b61f7b43de5db9bab
btstevens <brad.stevens@gmail.com>
2020-04-07 15:41:16 -0600

    localadmin_check

--- jumpcloud-ADMU/Powershell/Start-JCADMU.ps1
+++ jumpcloud-ADMU/Powershell/Start-JCADMU.ps1
@@ -3,6 +3,13 @@
 $scriptPath = (Split-Path -Path:($MyInvocation.MyCommand.Path))
 # Load functions
 . ($scriptPath + '\Functions.ps1')
+# Check runningaslocaladmin
+If (([bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")) -eq $false)
+{
+    Write-Log 'ADMU must be ran as a local administrator..please correct & try again'
+    Read-Host -Prompt "Press Enter to exit"
+    exit
+}
 # Load form
 $formResults = Invoke-Expression -Command:('. "' + $scriptPath + '\Form.ps1"')
 # Send form results to process if $formresults & securechannel true
