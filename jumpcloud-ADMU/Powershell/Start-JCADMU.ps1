626db75d23a0f72918d9fca15defa10208a664f2
btstevens <brad.stevens@gmail.com>
2020-04-07 19:58:42 -0600

    domain_join_check

--- jumpcloud-ADMU/Powershell/Start-JCADMU.ps1
+++ jumpcloud-ADMU/Powershell/Start-JCADMU.ps1
@@ -1,12 +1,26 @@
-# Get script path
-$scriptPath = (Split-Path -Path:($MyInvocation.MyCommand.Path))
+# Check powershell version
+if ((Get-Host | Select-Object Version).version.major -ne 5) {
+    Write-Host 'ADMU must be ran on Windows Powershell version 5.1..please correct & try again'
+    Read-Host -Prompt "Press Enter to exit"
+    exit
+}
 # Check runningaslocaladmin
-If (([bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")) -eq $false)
+elseif (([bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")) -eq $false)
 {
     Write-Host 'ADMU must be ran as a local administrator..please correct & try again'
     Read-Host -Prompt "Press Enter to exit"
     exit
 }
+Write-Host 'Loading Jumpcloud ADMU. Please Wait.. Checking Domain Join Status'
+$WmiComputerSystem = Get-WmiObject -Class:('Win32_ComputerSystem')
+elseif (
+If (!$WmiComputerSystem.PartOfDomain) {
+    Write-Host 'ADMU must be ran on a domain joined system..please correct & try again'
+    Read-Host -Prompt "Press Enter to exit"
+    exit
+}
+# Get script path
+$scriptPath = (Split-Path -Path:($MyInvocation.MyCommand.Path))
 # Load functions
 . ($scriptPath + '\Functions.ps1')
 # Load form
