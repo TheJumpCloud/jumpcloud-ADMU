97f8101bf3ff439af95d1173c1eb8a248a9dca6a
btstevens <brad.stevens@gmail.com>
2020-04-13 20:15:00 -0600

    convertsid_unknownaccount

--- jumpcloud-ADMU/Powershell/Functions.ps1
+++ jumpcloud-ADMU/Powershell/Functions.ps1
@@ -323,12 +323,14 @@ function ConvertSID
   [CmdletBinding()]
   param
   (
-    [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
-    $Sid
+      [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+      $Sid
   )
-  process
-  {
+  try {
     (New-Object System.Security.Principal.SecurityIdentifier($Sid)).Translate( [System.Security.Principal.NTAccount]).Value
+  }
+  catch {
+    return 'UNKNOWN ACCOUNT'
   }
 }
 #endregion Functions
