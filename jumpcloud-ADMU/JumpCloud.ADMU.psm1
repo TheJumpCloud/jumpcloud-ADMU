b8f1deb5cec3cb6994af208306352b0077523192
btstevens <brad.stevens@gmail.com>
2020-07-27 13:00:53 -0600

    Functions.ps1

--- jumpcloud-ADMU/JumpCloud.ADMU.psm1
+++ jumpcloud-ADMU/JumpCloud.ADMU.psm1
@@ -1,12 +1,17 @@
 # Load all functions from public and private folders
 #$Public = @( Get-ChildItem -Path "$PSScriptRoot/Public/*.ps1" -Recurse )
 #$Private = @( Get-ChildItem -Path "$PSScriptRoot/Private/*.ps1" -Recurse)
-$Public = @(Get-ChildItem -Path "$PSScriptRoot/Powershell/*.ps1" -Recurse)
-Foreach ($Import in @($Public + $Private)) {
-    Try {
-        . $Import.FullName
-    }
-    Catch {
-        Write-Error -Message "Failed to import function $($Import.FullName): $_"
-    }
-}
+$Public = @(Get-ChildItem -Path "$PSScriptRoot\Powershell\Functions.ps1")
+
+Write-Host $PSScriptRoot
+. "$PSScriptRoot\Powershell\Functions.ps1"
+
+# Foreach ($Import in @($Public + $Private)) {
+#     Try {
+#         Write-Host $Import.FullName
+#         . $Import.FullName
+#     }
+#     Catch {
+#         Write-Error -Message "Failed to import function $($Import.FullName): $_"
+#     }
+# }
