919c610d279d1310a37c205b243cd5c931f43ec6
btstevens <brad.stevens@gmail.com>
2020-07-30 08:44:46 -0600

    build_help_files

---
+++ Deploy/Build-HelpFiles.ps1
@@ -0,0 +1,26 @@
+. ($PSScriptRoot + '/' + 'Get-Config.ps1')
+###########################################################################
+Write-Host ('[status]Importing current module: ' + $ModuleName)
+Import-Module ($FilePath_psd1) -Force
+# Install module onto system
+If (-not (Get-InstalledModule -Name:('PlatyPS') -ErrorAction SilentlyContinue)) { Install-Module -Force -Name:('PlatyPS') }
+# Import module into session
+If (-not (Get-Module -Name:('PlatyPS'))) { Import-Module -Force -Name:('PlatyPS') }
+Write-Host ('[status]Creating/Updating help files')
+$Functions_Public | ForEach-Object {
+    $FunctionName = $_.BaseName
+    $FilePath_Md = $FolderPath_Docs + '/' + $FunctionName + '.md'
+    If (Test-Path -Path:($FilePath_Md))
+    {
+        # Write-Host ('Updating: ' + $FunctionName + '.md')
+        Update-MarkdownHelp -Path:($FilePath_Md) -Force -ExcludeDontShow -UpdateInputOutput -UseFullTypeName
+    }
+    Else
+    {
+        # Write-Host ('Creating: ' + $FunctionName + '.md')
+        New-MarkdownHelp  -Command:($FunctionName) -OutputFolder:($FolderPath_Docs) -Force -ExcludeDontShow -OnlineVersionUrl:($GitHubWikiUrl + $FunctionName) -UseFullTypeName
+    }
+}
+# Create new ExternalHelp file.
+Write-Host ('[status]Creating new external help file')
+New-ExternalHelp -Path:($FolderPath_Docs) -OutputPath:($FolderPath_enUS) -Force
