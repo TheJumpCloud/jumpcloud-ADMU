c51e5ca9a7b0ac33ca1803a3fb142762f9b702e1
btstevens <brad.stevens@gmail.com>
2020-07-30 16:50:44 -0600

    kill_installer

--- Deploy/TestSetup.ps1
+++ Deploy/TestSetup.ps1
@@ -35,10 +35,8 @@ $JumpCloudConnectKey = $TestOrgConnectKey
 
 #Prechecks
 #check if installer is stuck running and kill
-if ((Get-Process -PID 1588) -or (Get-Process -PID 2824)){
-        taskkill.exe /F /PID 1588
-        taskkill.exe /F /PID 2824
-}
+$process = get-process JumpCloudInstaller
+$process.kill
 #Clear Temp\JCADMU folder
 if ((Test-Path 'C:\Windows\Temp\JCADMU') -eq $true){
     remove-item -Path 'C:\windows\Temp\JCADMU' -Force -Recurse
