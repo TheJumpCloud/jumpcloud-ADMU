221e1dc7a08491894a5850e89149747f4d0208ea
Joe Workman <joe.workman@jumpcloud.com>
2020-11-25 12:00:23 -0700

    add output to uwp apps

--- Deploy/uwp_fix_jcadmu.ps1
+++ Deploy/uwp_fix_jcadmu.ps1
@@ -1,11 +1,12 @@
 $ADMUKEY = "HKCU:\SOFTWARE\JCADMU"
 if (Get-Item $ADMUKEY -ErrorAction SilentlyContinue) {
 
-$appxmanifest = ($HOME + '\AppData\Local\JumpCloudADMU\appx_manifest.csv')
-$newList = Import-CSV $appxmanifest
-foreach ($item in $newlist){
-   Add-AppxPackage -DisableDevelopmentMode -Register "$($item.InstallLocation)\AppxManifest.xml"
-}
+   $appxmanifest = ($HOME + '\AppData\Local\JumpCloudADMU\appx_manifest.csv')
+   $newList = Import-CSV $appxmanifest
+   $output = foreach ($item in $newlist) {
+      Add-AppxPackage -DisableDevelopmentMode -Register "$($item.InstallLocation)\AppxManifest.xml"
+   }
+   $output | Out-File "$HOME + '\AppData\Local\JumpCloudADMU\appx_manifestLog.txt"
 }
 else{
      exit
