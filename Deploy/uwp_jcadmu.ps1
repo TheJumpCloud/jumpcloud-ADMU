dd71ae382cd8a142df84145e33e211e231e946ef
Joe Workman <joe.workman@jumpcloud.com>
2020-11-30 17:31:44 -0700

    log streams from appxpackage to output file

--- Deploy/uwp_jcadmu.ps1
+++ Deploy/uwp_jcadmu.ps1
@@ -3,8 +3,9 @@ if (Get-Item $ADMUKEY -ErrorAction SilentlyContinue) {
 
    $appxmanifest = ($HOME + '\AppData\Local\JumpCloudADMU\appx_manifest.csv')
    $newList = Import-CSV $appxmanifest
-   $output = foreach ($item in $newlist) {
-      Add-AppxPackage -DisableDevelopmentMode -Register "$($item.InstallLocation)\AppxManifest.xml"
+   $output = @()
+   foreach ($item in $newlist) {
+      $output += Add-AppxPackage -DisableDevelopmentMode -Register "$($item.InstallLocation)\AppxManifest.xml" *>&1
    }
    $output | Out-File "$HOME\AppData\Local\JumpCloudADMU\appx_manifestLog.txt"
 }
