07d0ee91705ec2e9a18ae7761ba5cb64b2677e65
Brad Stevens <brad.stevens@gmail.com>
2021-02-23 12:46:06 -0700

    add_newlocaluser_function

--- jumpcloud-ADMU/Powershell/Start-Migration.ps1
+++ jumpcloud-ADMU/Powershell/Start-Migration.ps1
@@ -1,4 +1,145 @@
 #region Functions
+function New-LocalUser
+{
+    [CmdletBinding()]
+    [Alias()]
+    [OutputType([int])]
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        $userName,
+        # Param2 help description
+        [string]
+        $password
+    )
+
+    $system = [ADSI]"WinNT://$env:COMPUTERNAME";
+    $user = $system.Create("user",$userName);
+    $user.SetPassword($password);
+    $user.SetInfo();
+
+    $flag=$user.UserFlags.value -bor 0x10000;
+    $user.put("userflags",$flag);
+    $user.SetInfo();
+
+    $group = [ADSI]("WinNT://$env:COMPUTERNAME/Users");
+    $group.PSBase.Invoke("Add", $user.PSBase.Path);
+}
+
+#function to register a native method
+function Register-NativeMethod
+{
+    [CmdletBinding()]
+    [Alias()]
+    [OutputType([int])]
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        [string]$dll,
+
+        # Param2 help description
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=1)]
+        [string]
+        $methodSignature
+    )
+
+    $script:nativeMethods += [PSCustomObject]@{ Dll = $dll; Signature = $methodSignature; }
+}
+
+#function to add native method
+function Add-NativeMethods
+{
+    [CmdletBinding()]
+    [Alias()]
+    [OutputType([int])]
+    Param($typeName = 'NativeMethods')
+
+    $nativeMethodsCode = $script:nativeMethods | ForEach-Object { "
+        [DllImport(`"$($_.Dll)`")]
+        public static extern $($_.Signature);
+    " }
+
+    Add-Type @"
+        using System;
+        using System.Text;
+        using System.Runtime.InteropServices;
+        public static class $typeName {
+            $nativeMethodsCode
+        }
+"@
+}
+
+#Main function to create the new user profile
+function Create-NewProfile {
+
+  [CmdletBinding()]
+  [Alias()]
+  [OutputType([int])]
+  Param
+  (
+      # Param1 help description
+      [Parameter(Mandatory=$true,
+                 ValueFromPipelineByPropertyName=$true,
+                 Position=0)]
+      [string]$UserName,
+
+      # Param2 help description
+      [Parameter(Mandatory=$true,
+                 ValueFromPipelineByPropertyName=$true,
+                 Position=1)]
+      [string]
+      $Password
+  )
+
+  Write-Verbose "Creating local user $Username";
+
+  try
+  {
+      New-LocalUser -username $UserName -password $Password;
+  }
+  catch
+  {
+      Write-Error $_.Exception.Message;
+      break;
+  }
+  $methodName = 'UserEnvCP'
+  $script:nativeMethods = @();
+
+  if (-not ([System.Management.Automation.PSTypeName]$MethodName).Type)
+  {
+      Register-NativeMethod "userenv.dll" "int CreateProfile([MarshalAs(UnmanagedType.LPWStr)] string pszUserSid,`
+       [MarshalAs(UnmanagedType.LPWStr)] string pszUserName,`
+       [Out][MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszProfilePath, uint cchProfilePath)";
+
+      Add-NativeMethods -typeName $MethodName;
+  }
+
+  $localUser = New-Object System.Security.Principal.NTAccount("$UserName");
+  $userSID = $localUser.Translate([System.Security.Principal.SecurityIdentifier]);
+  $sb = new-object System.Text.StringBuilder(260);
+  $pathLen = $sb.Capacity;
+
+  Write-Verbose "Creating user profile for $Username";
+
+  try
+  {
+      [UserEnvCP]::CreateProfile($userSID.Value, $Username, $sb, $pathLen) | Out-Null;
+  }
+  catch
+  {
+      Write-Error $_.Exception.Message;
+      break;
+  }
+}
+
 function enable-privilege {
   param(
     ## The privilege to adjust. This set is taken from
@@ -5998,22 +6139,24 @@ Function Start-Migration {
         # $remoteRun = $false
         Write-Log -Message:('Creating New Local User ' + $localComputerName + '\' + $JumpCloudUserName)
         #Create New User
-        $newUserPassword = ConvertTo-SecureString -String $TempPassword -AsPlainText -Force
-        $userMessage = new-localUser -Name $JumpCloudUserName -password $newUserPassword *>&1
-        $userExitCode = $?
-        if ($userExitCode -ne $true) {
-          Write-log -Message:("$userMessage")
-          Write-log -Message:("The user: $JumpCloudUserName could not be created, exiting")
-          exit
-        }
-        Write-Log -Message:('Spawning process for new profile')
-        $user = "$env:COMPUTERNAME\$JumpCloudUserName"
-        $MyPlainTextString = $TempPassword
-        $MySecureString = ConvertTo-SecureString -String $MyPlainTextString -AsPlainText -Force
-        $Credential = New-Object System.Management.Automation.PSCredential $user, $MySecureString
-        Start-Process Powershell.exe -Credential $Credential -WorkingDirectory "$windowsDrive\windows\System32" -ArgumentList ('-WindowStyle Hidden')
-        start-sleep 1
-        TASKKILL.exe /F /FI "USERNAME eq $JumpCloudUserName"
+        Create-NewProfile -UserName $jumpcloudusername -Password $TempPassword 
+
+        # $newUserPassword = ConvertTo-SecureString -String $TempPassword -AsPlainText -Force
+        # $userMessage = new-localUser -Name $JumpCloudUserName -password $newUserPassword *>&1
+        # $userExitCode = $?
+        # if ($userExitCode -ne $true) {
+        #   Write-log -Message:("$userMessage")
+        #   Write-log -Message:("The user: $JumpCloudUserName could not be created, exiting")
+        #   exit
+        # }
+        # Write-Log -Message:('Spawning process for new profile')
+        # $user = "$env:COMPUTERNAME\$JumpCloudUserName"
+        # $MyPlainTextString = $TempPassword
+        # $MySecureString = ConvertTo-SecureString -String $MyPlainTextString -AsPlainText -Force
+        # $Credential = New-Object System.Management.Automation.PSCredential $user, $MySecureString
+        # Start-Process Powershell.exe -Credential $Credential -WorkingDirectory "$windowsDrive\windows\System32" -ArgumentList ('-WindowStyle Hidden')
+        # start-sleep 1
+        # TASKKILL.exe /F /FI "USERNAME eq $JumpCloudUserName"
       }
       Write-Log -Message:('Creating Backup of User Registry Hive')
       $olduserprofileimagepath = Get-ItemPropertyValue -Path ('HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\' + $SelectedUserSID) -Name 'ProfileImagePath'
