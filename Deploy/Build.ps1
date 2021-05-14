a597e9fc243f83c5cb685a122261aa2ee21b2f56
Brad Stevens <brad.stevens@gmail.com>
2021-05-14 13:00:53 -0600

    test1

--- Deploy/Build.ps1
+++ Deploy/Build.ps1
@@ -35,7 +35,7 @@ If (-not [System.String]::IsNullOrEmpty($NewContent))
     $Version = Select-String -Path:($FormPath) -Pattern:($VersionRegex)
     If (-not [System.String]::IsNullOrEmpty($Version))
     {
-    Invoke-ps2exe -inputFile ($RootPath + '\Deploy\ADMU.ps1') -outputFile ($RootPath + '\jumpcloud-ADMU\exe\gui_jcadmu.exe') -title 'JumpCloud ADMU' -product 'JumpCloud ADMU' -description 'JumpCloud AD Migration Utility' -copyright '(c) 2021' -version $Version.Matches.Value -company 'JumpCloud' -requireAdmin -iconfile ($RootPath + '\Deploy\admu.ico')
+    #Invoke-ps2exe -inputFile ($RootPath + '\Deploy\ADMU.ps1') -outputFile ($RootPath + '\jumpcloud-ADMU\exe\gui_jcadmu.exe') -title 'JumpCloud ADMU' -product 'JumpCloud ADMU' -description 'JumpCloud AD Migration Utility' -copyright '(c) 2021' -version $Version.Matches.Value -company 'JumpCloud' -requireAdmin -iconfile ($RootPath + '\Deploy\admu.ico')
     Write-Host "gui_jcadmu.exe was generated successfully"
     }
     Else
