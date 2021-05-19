6122725c6466017fd4eae3b3381b9ec185050270
Joe Workman <joe.workman@jumpcloud.com>
2021-05-19 09:16:31 -0600

    revert invoke pester

--- jumpcloud-ADMU/Powershell/InvokePester.ps1
+++ jumpcloud-ADMU/Powershell/InvokePester.ps1
@@ -5,20 +5,20 @@
 Import-Module -Name Pester
 
 # Run Pester tests
-$PesterResults = Invoke-Pester -Script ($PSScriptRoot + '/Tests/')
-$FailedTests = $PesterResults.TestResult | Where-Object { $_.Passed -eq $false }
-If ($FailedTests)
-{
-    Write-Output ('')
-    Write-Output ('###########################################################')
-    Write-Output ('#################### Error Description ####################')
-    Write-Output ('###########################################################')
-    Write-Output ('')
-    $FailedTests | ForEach-Object { $_.Name + '; ' + $_.FailureMessage + '; ' }
-    Write-Error -Message:('Tests Failed: ' + [string]($FailedTests | Measure-Object).Count)
-}
+# $PesterResults = Invoke-Pester -Script ($PSScriptRoot + '/Tests/') -PassThru
+# $FailedTests = $PesterResults.TestResult | Where-Object { $_.Passed -eq $false }
+# If ($FailedTests)
+# {
+#     Write-Output ('')
+#     Write-Output ('###########################################################')
+#     Write-Output ('#################### Error Description ####################')
+#     Write-Output ('###########################################################')
+#     Write-Output ('')
+#     $FailedTests | ForEach-Object { $_.Name + '; ' + $_.FailureMessage + '; ' }
+#     Write-Error -Message:('Tests Failed: ' + [string]($FailedTests | Measure-Object).Count)
+# }
 
 # Run Pester tests
 # $PesterResultsFileXml = $PSScriptRoot + '/Pester.Tests.Results.xml'
-# Invoke-Pester -Script ($PSScriptRoot + '/Tests/')
+Invoke-Pester -Script ($PSScriptRoot + '/Tests/')
 Write-Host -ForegroundColor Green '-------------Done-------------'
