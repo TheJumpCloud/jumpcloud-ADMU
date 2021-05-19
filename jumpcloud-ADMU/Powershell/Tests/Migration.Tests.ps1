1e2ad9ad76426f4424dcce2cf33e09f630802785
jworkmanjc <joe.workman@jumpcloud.com>
2021-05-19 12:20:20 -0600

    migration tests

--- jumpcloud-ADMU/Powershell/Tests/Migration.Tests.ps1
+++ jumpcloud-ADMU/Powershell/Tests/Migration.Tests.ps1
@@ -1,11 +1,11 @@
 # Dotsouce Variables for Testing:
-. $PSScriptRoot\BuildVariables.ps1
-. $PSScriptRoot\..\Start-Migration.ps1
 
+BeforeAll{
+    . $PSScriptRoot\BuildVariables.ps1
+    . $PSScriptRoot\..\Start-Migration.ps1
+    . $PSScriptRoot\SetupAgent.ps1
+}
 Describe 'Migration Test Scenarios'{
-    BeforeAll{
-        .\SetupAgent.ps1
-    }
     Context 'Start-Migration on local accounts (Test Functionallity)' {
         It "username extists for testing" {
             foreach ($user in $userTestingHash.Values){
@@ -17,9 +17,9 @@ Describe 'Migration Test Scenarios'{
         It "Test Migration for Local user" {
             foreach ($user in $userTestingHash.Values)
             {
-                . $PSScriptRoot/../../Start-Migration.ps1
-                # Invoke-Command -ScriptBlock { Start-Migration -JumpCloudUserName "$($user.JCUsername)" -SelectedUserName "$($user.username)" -TempPassword "$($user.password)"} | Should -Not Throw
-                Start-Migration -JumpCloudUserName "$($user.JCUsername)" -SelectedUserName "$($user.username)" -TempPassword "$($user.password)" | Should -Not Throw
+                write-host "Running: Start-Migration -JumpCloudUserName $($user.JCUsername) -SelectedUserName $($user.username) -TempPassword $($user.password)"
+                Invoke-Command -ScriptBlock { Start-Migration -JumpCloudUserName "$($user.JCUsername)" -SelectedUserName "$ENV:COMPUTERNAME\$($user.username)" -TempPassword "$($user.password)"} | Should -Not -Throw
+                # Start-Migration -JumpCloudUserName "$($user.JCUsername)" -SelectedUserName "$ENV:COMPUTERNAME\$($user.username)" -TempPassword "$($user.password)" | Should -Not Throw
             }
         }
     }
