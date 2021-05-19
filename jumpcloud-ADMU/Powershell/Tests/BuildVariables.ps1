2eafc4d304af16cbd6fed2ddf87be5eb14b877bb
jworkmanjc <joe.workman@jumpcloud.com>
2021-05-19 13:11:54 -0600

    build vars update, can't account for dup local names

--- jumpcloud-ADMU/Powershell/Tests/BuildVariables.ps1
+++ jumpcloud-ADMU/Powershell/Tests/BuildVariables.ps1
@@ -3,27 +3,27 @@ $testUserPassword = '$T#st1234'
 # Region for test user generation
 $userTestingHash = @{
     testCase1 = @{
-        Description = 'Test that user can be converted'
+        Description = 'Test that user can be converted with different name'
         Username    = 'user.name';
         UserSID     = '';
         Password    = $testUserPassword;
-        JCUsername  = 'User.Name';
+        JCUsername  = 'Username.username';
         JCUserSID   = '';
     }
     testCase2 = @{
-        Description = 'Test that user can be converted twice for random errors'
+        Description = 'Test that user can be converted with different name x2'
         Username    = 'username';
         UserSID     = '';
         Password    = $testUserPassword;
         JCUsername  = 'newUsername';
         JCUserSID   = '';
     }
-    testCase3 = @{
-        Description = 'Test that user can be converted with same name'
-        Username    = 'sameUsername';
-        UserSID     = '';
-        Password    = $testUserPassword;
-        JCUsername  = 'sameUsername';
-        JCUserSID   = '';
-    }
+    # testCase3 = @{
+    #     Description = 'Test that user can be converted with same name'
+    #     Username    = 'sameUsername';
+    #     UserSID     = '';
+    #     Password    = $testUserPassword;
+    #     JCUsername  = 'sameUsername';
+    #     JCUserSID   = '';
+    # }
 }
