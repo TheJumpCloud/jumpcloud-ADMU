da9881fa7b50c78d78957018815fc431f3ee5be0
Joe Workman <joe.workman@jumpcloud.com>
2021-05-19 08:42:47 -0600

    pester errors

--- jumpcloud-ADMU/Powershell/InvokePester.ps1
+++ jumpcloud-ADMU/Powershell/InvokePester.ps1
@@ -1,24 +1,24 @@
 # Load functions
 . ($PSScriptRoot + '/Start-Migration.ps1')
 
-# # Run Pester tests
-# $PesterResults = Invoke-Pester -Script:(@{ Path = $PSScriptRoot + '/Tests/'; }) -PassThru
-# $FailedTests = $PesterResults.TestResult | Where-Object { $_.Passed -eq $false }
-# If ($FailedTests)
-# {
-#     Write-Output ('')
-#     Write-Output ('##############################################################################################################')
-#     Write-Output ('##############################Error Description###############################################################')
-#     Write-Output ('##############################################################################################################')
-#     Write-Output ('')
-#     $FailedTests | ForEach-Object { $_.Name + '; ' + $_.FailureMessage + '; ' }
-#     Write-Error -Message:('Tests Failed: ' + [string]($FailedTests | Measure-Object).Count)
-# }
-
 # Import
 Import-Module -Name Pester
 
 # Run Pester tests
-$PesterResultsFileXml = $PSScriptRoot + '/Pester.Tests.Results.xml'
-Invoke-Pester -Script ($PSScriptRoot + '/Tests/')
+$PesterResults = Invoke-Pester -Script:(@{ Path = $PSScriptRoot + '/Tests/'; }) -PassThru
+$FailedTests = $PesterResults.TestResult | Where-Object { $_.Passed -eq $false }
+If ($FailedTests)
+{
+    Write-Output ('')
+    Write-Output ('###########################################################')
+    Write-Output ('#################### Error Description ####################')
+    Write-Output ('###########################################################')
+    Write-Output ('')
+    $FailedTests | ForEach-Object { $_.Name + '; ' + $_.FailureMessage + '; ' }
+    Write-Error -Message:('Tests Failed: ' + [string]($FailedTests | Measure-Object).Count)
+}
+
+# Run Pester tests
+# $PesterResultsFileXml = $PSScriptRoot + '/Pester.Tests.Results.xml'
+# Invoke-Pester -Script ($PSScriptRoot + '/Tests/')
 Write-Host -ForegroundColor Green '-------------Done-------------'
