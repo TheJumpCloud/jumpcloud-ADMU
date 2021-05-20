54a34030ce200dea120c1d5eebb38eb7661c33c9
Brad Stevens <brad.stevens@gmail.com>
2021-05-20 16:38:16 -0600

    missing_\

--- jumpcloud-ADMU/Powershell/InvokePester.ps1
+++ jumpcloud-ADMU/Powershell/InvokePester.ps1
@@ -7,7 +7,7 @@ Import-Module -Name Pester
 # Run Pester tests
 $PesterResultsFileXmldir = ($HOME + '\project\jumpcloud-ADMU\test-results\')
 new-item -path $PesterResultsFileXmldir -ItemType Directory
-$PesterResults = Invoke-Pester -Script ($HOME + 'project\jumpcloud-ADMU\Powershell\Tests\') -PassThru -OutputFile ($PesterResultsFileXmldir + 'results.xml')
+$PesterResults = Invoke-Pester -Script ($HOME + '\project\jumpcloud-ADMU\Powershell\Tests\') -PassThru -OutputFile ($PesterResultsFileXmldir + 'results.xml')
 $FailedTests = $PesterResults.TestResult | Where-Object { $_.Passed -eq $false }
 If ($FailedTests)
 {
