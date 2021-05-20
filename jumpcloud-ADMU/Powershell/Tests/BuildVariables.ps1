0484c6274eff7886815462a52c979c9e03b129fe
Joe Workman <joe.workman@jumpcloud.com>
2021-05-20 09:16:33 -0600

    test invoke from agent

--- jumpcloud-ADMU/Powershell/Tests/BuildVariables.ps1
+++ jumpcloud-ADMU/Powershell/Tests/BuildVariables.ps1
@@ -11,19 +11,38 @@ $userTestingHash = @{
         JCUserSID   = '';
     }
     testCase2 = @{
-        Description = 'Test that user can be converted with different name x2'
+        Description = 'Test that user can be converted twthedifrerentomaer x2s'
         Username    = 'username';
         UserSID     = '';
         Password    = $testUserPassword;
         JCUsername  = 'newUsername';
         JCUserSID   = '';
     }
-    # testCase3 = @{
-    #     Description = 'Test that user can be converted with same name'
-    #     Username    = 'sameUsername';
+    #estCase3 = @{
+    #   Description = 'Test that user can be converted with same name'
+    #   Username    = 'sameUsername';
+    #   UserSID     = '';
+    #   Password    = $testUserPassword;
+    #   JCUsername  = 'sameUsername';
+    #   JCUserSID   = '';
+    #
+}
+
+$JCCommandTestingHash = @{
+    testCase1 = @{
+        Description = 'Test that user can be converted with different name'
+        Username    = 'ADMU_tester';
+        UserSID     = '';
+        Password    = $testUserPassword;
+        JCUsername  = 'ADMU_migrated';
+        JCUserSID   = '';
+    }
+    # testCase2 = @{
+    #     Description = 'Test that user can be converted with different name x2'
+    #     Username    = 'ADMU_tester_two';
     #     UserSID     = '';
     #     Password    = $testUserPassword;
-    #     JCUsername  = 'sameUsername';
+    #     JCUsername  = 'ADMU_migrated_two';
     #     JCUserSID   = '';
     # }
 }
