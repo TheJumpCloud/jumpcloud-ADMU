0ce7a79154e01c279ec8735b97dad107b4d8682f
Joe Workman <joe.workman@jumpcloud.com>
2021-06-01 14:43:37 -0600

    Turn psscriptanalyzer into a testable test

--- jumpcloud-ADMU/Powershell/Tests/PSScriptAnalyzer.Tests.ps1
+++ jumpcloud-ADMU/Powershell/Tests/PSScriptAnalyzer.Tests.ps1
@@ -1,10 +1,12 @@
-$FolderPath_Module = (Get-Item -Path($PSScriptRoot)).Parent.FullName
-$SettingsFile = "$PSScriptRoot\PSScriptAnalyzerSettings.psd1"
-# Import Settings:
-$SettingsFromFile = Import-PowerShellDataFile $SettingsFile
-$settingsObject = @{
-    Severity = $SettingsFromFile.Severity
-    ExcludeRules = $SettingsFromFile.ExcludeRules
+BeforeAll{
+    $FolderPath_Module = (Get-Item -Path($PSScriptRoot)).Parent.FullName
+    $SettingsFile = "$PSScriptRoot\PSScriptAnalyzerSettings.psd1"
+    # Import Settings:
+    $SettingsFromFile = Import-PowerShellDataFile $SettingsFile
+    $settingsObject = @{
+        Severity = $SettingsFromFile.Severity
+        ExcludeRules = $SettingsFromFile.ExcludeRules
+    }
 }
 
 ################################################################################
@@ -18,17 +20,33 @@ $settingsObject = @{
 # 'PSAvoidUsingInvokeExpression' #TODO: Description
 ################################################################################
 
-Write-Host ('[status]Running PSScriptAnalyzer on: ' + $FolderPath_Module)
-Write-Host ('[status]PSScriptAnalyzer Settings File: ' + $SettingsFile)
+Describe 'PSScriptAnalyzer Test Suite' {
+    Context 'PSScriptAnalyzer Tests'{
+        BeforeAll{
+            Write-Host ('[status]Running PSScriptAnalyzer on: ' + $FolderPath_Module)
+            Write-Host ('[status]PSScriptAnalyzer Settings File: ' + $SettingsFile)
+            $ScriptAnalyzerResults = Invoke-ScriptAnalyzer -Path:($FolderPath_Module) -Recurse -Settings $settingsObject -ReportSummary
+            If (-not [System.String]::IsNullOrEmpty($ScriptAnalyzerResults))
+            {
+                $ScriptAnalyzerResults | ForEach-Object {
+                    Write-Error ('[PSScriptAnalyzer][' + $_.Severity + '][' + $_.RuleName + '] ' + $_.Message + ' found in "' + $_.ScriptPath + '" at line ' + $_.Line + ':' + $_.Column)
+                }
+            }
+            Else
+            {
+                Write-Host ('[success]ScriptAnalyzer returned no results')
+            }
+        }
+        It 'PSScriptAnalyzer Results should be null'{
+            $ScriptAnalyzerResults | Should -BeNullOrEmpty
+        }
+        It 'PSScriptAnalyzer SettingsFile should exist' {
+            test-path $SettingsFile | Should -Be $true
+        }
+        It 'PSScriptAnalyzer SettingsObject Should Not Be Null or Empty'{
+            $SettingsFromFile | Should -Not -BeNullOrEmpty
+            $settingsObject | Should -Not -BeNullOrEmpty
+        }
 
-$ScriptAnalyzerResults = Invoke-ScriptAnalyzer -Path:($FolderPath_Module) -Recurse -Settings $settingsObject # -ExcludeRule PSAvoidUsingPlainTextForPassword, PSAvoidOverwritingBuiltInCmdlets, PSAvoidUsingConvertToSecureStringWithPlainText
-If (-not [System.String]::IsNullOrEmpty($ScriptAnalyzerResults))
-{
-    $ScriptAnalyzerResults | ForEach-Object {
-        Write-Error ('[PSScriptAnalyzer][' + $_.Severity + '][' + $_.RuleName + '] ' + $_.Message + ' found in "' + $_.ScriptPath + '" at line ' + $_.Line + ':' + $_.Column)
     }
-}
-Else
-{
-    Write-Host ('[success]ScriptAnalyzer returned no results')
 }
