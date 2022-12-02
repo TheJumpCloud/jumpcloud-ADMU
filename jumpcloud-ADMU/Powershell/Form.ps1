d76a4a72d9329e663ecd1acfa07a4ec3409bcddc
Geoffrey Wein <geoffrey.wein@jumpcloud.com>
2022-12-02 16:40:18 -0700

    disable cb_leavedomain - AzureAD Joined

--- jumpcloud-ADMU/Powershell/Form.ps1
+++ jumpcloud-ADMU/Powershell/Form.ps1
@@ -182,7 +182,7 @@ function show-mtpSelection {
                 <CheckBox Name="cb_installjcagent" Content="Install JCAgent" HorizontalAlignment="Left" Margin="123,101,0,0" VerticalAlignment="Top" FontWeight="Normal" IsChecked="False"/>
                 <CheckBox Name="cb_bindAsAdmin" Content="Bind As Admin" HorizontalAlignment="Left" Margin="253,101,0,0" VerticalAlignment="Top" FontWeight="Normal" IsChecked="False" IsEnabled="False"/>
                 <CheckBox Name="cb_leavedomain" Content="Leave Domain" HorizontalAlignment="Left" Margin="10,123,0,0" VerticalAlignment="Top" FontWeight="Normal" IsChecked="False"/>
-                <CheckBox Name="cb_autobindjcuser" Content="Autobind JC User" HorizontalAlignment="Left" Margin="123,123,0,0" VerticalAlignment="Top" FontWeight="Normal" IsChecked="False"/>
+                <CheckBox Name="cb_autobindjcuser" Content="Autobind JC User" HorizontalAlignment="Left" Margin="123,123,0,0" VerticalAlignment="Top" FontWeight="Normal" IsChecked="False" ToolTip=""/>
                 <Image Name="img_ckey_info" HorizontalAlignment="Left" Height="14" Margin="157,13,0,0" VerticalAlignment="Top" Width="14" Visibility="Hidden" ToolTip="The Connect Key provides you with a means of associating this system with your JumpCloud organization. The Key is used to deploy the agent to this system." />
                 <Image Name="img_ckey_valid" HorizontalAlignment="Left" Height="14" Margin="454,13,0,0" VerticalAlignment="Top" Width="14" Visibility="Hidden" ToolTip="Connect Key must be 40chars &amp; not contain spaces" />
                 <Image Name="img_apikey_info" HorizontalAlignment="Left" Height="14" Margin="157,42,0,0" VerticalAlignment="Top" Width="14" Visibility="Hidden" ToolTip="Click the link for more info on how to obtain the api key. The API key must be from a user with at least 'Manager' or 'Administrator' privileges." RenderTransformOrigin="1.857,-1.066"/>
@@ -538,6 +538,14 @@ $cb_autobindjcuser.Add_Unchecked( {
 $script:LeaveDomain = $false
 $cb_leavedomain.Add_Checked( { $script:LeaveDomain = $true })
 $cb_leavedomain.Add_Unchecked( { $script:LeaveDomain = $false })
+
+# If system is joined to AzureAD, disable Leave Domain checkbox
+if ($AzureADStatus -eq "YES") {
+    $cb_leavedomain.IsEnabled = $false
+    $cb_leavedomain.Add_Unchecked( { $script:LeaveDomain = $false })
+    $cb_leavedomain.ToolTip = "Unable to automatically leave domain due to being AzureAD Joined"
+    $cb_leavedomain.ToolTip.Add_PreviewMouseDown( { [System.Diagnostics.Process]::start("https://github.com/TheJumpCloud/jumpcloud-ADMU/wiki/Leaving-AzureAD-Domains") })
+}
 
 # Force Reboot checkbox
 $script:ForceReboot = $false
