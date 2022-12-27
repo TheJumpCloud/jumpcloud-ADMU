5dc56913649de17545e645e2ee23bdef9d03c7a5
Joe Workman <joe.workman@jumpcloud.com>
2022-12-27 10:47:45 -0500

    move publish to ps_gallery step

--- Deploy/Build-Module.ps1
+++ Deploy/Build-Module.ps1
@@ -53,11 +53,5 @@ If (!(($ModuleChangelog | Select-Object -First 1) -match $ModuleVersion)) {
 }
 # EndRegion Updating module change log
 # Begin Update Manifest Region
-
-$files = @(
-    "$PSScriptRoot\..\jumpcloud-ADMU\JumpCloud.ADMU.psd1"
-    "$PSScriptRoot\..\jumpcloud-ADMU\JumpCloud.ADMU.psm1"
-    "$PSScriptRoot\..\jumpcloud-ADMU\PowerShell\Start-Migration.ps1"
-)
-New-FileCatalog -path $files  -CatalogFilePath "$PSScriptRoot\..\JumpCloud-ADMU\ADMU.cat" -CatalogVersion 2.0
+. $PSScriptRoot\Update-SecurityCatalog.ps1
 # EndRegion Manifest
