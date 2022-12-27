0da943b6a2b2329ab49c0b0f17eb0ddd0e435480
Joe Workman <joe.workman@jumpcloud.com>
2022-12-27 10:50:16 -0500

    add cataloge file

--- Deploy/Update-SecurityCatalog.ps1
+++ Deploy/Update-SecurityCatalog.ps1
@@ -2,6 +2,7 @@
 $files = @(
     "$PSScriptRoot\..\jumpcloud-ADMU\JumpCloud.ADMU.psd1"
     "$PSScriptRoot\..\jumpcloud-ADMU\JumpCloud.ADMU.psm1"
+    "$PSScriptRoot\..\jumpcloud-ADMU\ADMU.cat"
     "$PSScriptRoot\..\jumpcloud-ADMU\PowerShell\Start-Migration.ps1"
 )
 New-FileCatalog -path $files  -CatalogFilePath "$PSScriptRoot\..\JumpCloud-ADMU\ADMU.cat" -CatalogVersion 2.0
