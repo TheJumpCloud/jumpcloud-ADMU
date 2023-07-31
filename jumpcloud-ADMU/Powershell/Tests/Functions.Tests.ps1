a2a429197c96da084d16554b85f44053af7499af
Joe Workman <joe.workman@jumpcloud.com>
2023-07-31 09:12:56 -0600

    remove testing var

--- jumpcloud-ADMU/Powershell/Tests/Functions.Tests.ps1
+++ jumpcloud-ADMU/Powershell/Tests/Functions.Tests.ps1
@@ -526,7 +526,6 @@ Describe 'Functions' {
         It 'Should return false when a users hive permissions are correct' {
             $datUserFalse = "ADMU_DATPermission" + -join ((65..90) + (97..122) | Get-Random -Count 5 | ForEach-Object { [char]$_ })
             InitUser -UserName $datUserFalse -Password $Password
-            $datUserFalse = 'ADMU_bind_2'
             $filePaths = @("C:\Users\$datUserFalse\AppData\Local\Microsoft\Windows\UsrClass.dat", "C:\Users\$datUserFalse\NTUser.DAT")
             $requiredAccounts = @("SYSTEM", "Administrators", "$datUserFalse")
             foreach ($FilePath in $filePaths) {
