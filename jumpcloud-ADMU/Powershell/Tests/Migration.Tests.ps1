b867177566f8d51163caba97ca85bfc91dfe22f1
Joe Workman <joe.workman@jumpcloud.com>
2023-08-25 14:32:38 -0600

    test verbosity

--- jumpcloud-ADMU/Powershell/Tests/Migration.Tests.ps1
+++ jumpcloud-ADMU/Powershell/Tests/Migration.Tests.ps1
@@ -1,3 +1,18 @@
+function Enable-TestNameAsVariablePlugin {
+    & (get-module pester) {
+        $PluginParams = @{
+            Name               = "SaveTestNameToVariable"
+            EachTestSetupStart = {
+                $GLOBAL:TestName = $Context.Test.Name
+            }
+            EachTestTeardown   = {
+                $GLOBAL:TestName = $null
+            }
+        }
+        $state.Plugin += New-PluginObject @PluginParams
+    }
+}
+
 BeforeAll {
     # import build variables for test cases
     write-host "Importing Build Variables:"
@@ -35,6 +50,11 @@ BeforeAll {
     Get-JCuser -username "ADMU_*" | Remove-JCuser -Force
 }
 Describe 'Migration Test Scenarios' {
+    Enable-TestNameAsVariablePlugin
+    BeforeEach {
+        Write-Host "---------------------------"
+        Write-Host "Begin Test: $testName"
+    }
     Context 'Start-Migration on local accounts (Test Functionallity)' {
         It "username extists for testing" {
             foreach ($user in $userTestingHash.Values) {
@@ -404,106 +424,110 @@ Describe 'Migration Test Scenarios' {
             $logonUI.SelectedUserSID | Should -not -Be $UserSID
         }
     }
-}
-
-Context 'Start-Migration Fails to Bind JumpCloud User to System and throws error' {
-    It 'user bound to system after migration' {
-        Write-Host "`nBegin Test: Start-Migration Fails to Bind JumpCloud User to System and throws error"
-        $Password = "Temp123!"
-        $user1 = "ADMU_" + -join ((65..90) + (97..122) | Get-Random -Count 5 | ForEach-Object { [char]$_ })
-        $user2 = "ADMU_" + -join ((65..90) + (97..122) | Get-Random -Count 5 | ForEach-Object { [char]$_ })
-        InitUser -UserName $user1 -Password $Password
-        write-host "`nRunning: Start-Migration -JumpCloudUserName $($user2) -SelectedUserName $($user1) -TempPassword $($Password)`n"
-        { Start-Migration -JumpCloudAPIKey $env:JCApiKey -AutobindJCUser $true -JumpCloudUserName "$($user2)" -SelectedUserName "$ENV:COMPUTERNAME\$($user1)" -TempPassword "$($Password)" } | Should -Throw
-    }
-}
-Context 'Start-Migration kicked off through JumpCloud agent' {
-    BeforeAll {
-        # test connection to Org
-        $Org = Get-JcSdkOrganization
-        Write-Host "Connected to Pester Org: $($Org.DisplayName)"
-        # Get System Key
-        $config = get-content 'C:\Program Files\JumpCloud\Plugins\Contrib\jcagent.conf'
-        $regex = 'systemKey\":\"(\w+)\"'
-        $systemKey = [regex]::Match($config, $regex).Groups[1].Value
-        Write-Host "Running Tests on SystemID: $systemKey"
-        # Connect-JCOnline
-
-        # variables for test
-        $CommandBody = '
-        . "C:\Users\circleci.$env:COMPUTERNAME\project\jumpcloud-ADMU\Powershell\Start-Migration.ps1"
-        # Trim env vars with hardcoded ""
-        $JCU = ${ENV:$JcUserName}.Trim([char]0x0022)
-        $SU = ${ENV:$SelectedUserName}.Trim([char]0x0022)
-        $PW = ${ENV:$TempPassword}.Trim([char]0x0022)
-        Start-Migration -JumpCloudUserName $JCU -SelectedUserName $ENV:COMPUTERNAME\$SU -TempPassword $PW
-        '
-        $CommandTrigger = 'ADMU'
-        $CommandName = 'RemoteADMU'
-        # clear command results
-        $results = Get-JcSdkCommandResult
-        foreach ($result in $results) {
-            # Delete Command Results
-            Write-Host "Found Command Results: $($result.id) removing..."
-            remove-jcsdkcommandresult -id $result.id
+    Context 'Start-Migration Fails to Bind JumpCloud User to System and throws error' {
+        It 'user bound to system after migration' {
+            Write-Host "`nBegin Test: Start-Migration Fails to Bind JumpCloud User to System and throws error"
+            $Password = "Temp123!"
+            $user1 = "ADMU_" + -join ((65..90) + (97..122) | Get-Random -Count 5 | ForEach-Object { [char]$_ })
+            $user2 = "ADMU_" + -join ((65..90) + (97..122) | Get-Random -Count 5 | ForEach-Object { [char]$_ })
+            InitUser -UserName $user1 -Password $Password
+            write-host "`nRunning: Start-Migration -JumpCloudUserName $($user2) -SelectedUserName $($user1) -TempPassword $($Password)`n"
+            { Start-Migration -JumpCloudAPIKey $env:JCApiKey -AutobindJCUser $true -JumpCloudUserName "$($user2)" -SelectedUserName "$ENV:COMPUTERNAME\$($user1)" -TempPassword "$($Password)" } | Should -Throw
         }
-        # Clear previous commands matching the name
-        $RemoteADMUCommands = Get-JcSdkCommand | Where-Object { $_.name -eq $CommandName }
-        foreach ($result in $RemoteADMUCommands) {
-            # Delete Command Results
-            Write-Host "Found existing Command: $($result.id) removing..."
-            Remove-JcSdkCommand -id $result.id
-        }
+    }
+    Context 'Start-Migration kicked off through JumpCloud agent' {
+        BeforeAll {
+            # test connection to Org
+            $Org = Get-JcSdkOrganization
+            Write-Host "Connected to Pester Org: $($Org.DisplayName)"
+            # Get System Key
+            $config = get-content 'C:\Program Files\JumpCloud\Plugins\Contrib\jcagent.conf'
+            $regex = 'systemKey\":\"(\w+)\"'
+            $systemKey = [regex]::Match($config, $regex).Groups[1].Value
+            Write-Host "Running Tests on SystemID: $systemKey"
+            # Connect-JCOnline
 
-        # Create command & association to command
-        New-JcSdkCommand -Command $CommandBody -CommandType "windows" -Name $CommandName -Trigger $CommandTrigger -Shell powershell
-        $CommandID = (Get-JcSdkCommand | Where-Object { $_.Name -eq $CommandName }).Id
-        Write-Host "Setting CommandID: $CommandID associations"
-        Set-JcSdkCommandAssociation -CommandId $CommandID -Id $systemKey -Op add -Type system
-    }
-    It 'Test that system key exists' {
-        $systemKey | Should -Not -BeNullOrEmpty
-    }
-    It 'Invoke ADMU from JumpCloud Command' {
-        # clear results
-        $results = Get-JcSdkCommandResult
-        foreach ($result in $results) {
-            # Delete Command Results
-            remove-jcsdkcommandresult -id $result.id
-        }
-        # begin tests
-        foreach ($user in $JCCommandTestingHash.Values) {
-            write-host "Running: Start-Migration -JumpCloudUserName $($user.JCUsername) -SelectedUserName $($user.username) -TempPassword $($user.password)"
-            $headers = @{
-                'Accept'    = "application/json"
-                'x-api-key' = $env:JCApiKey
+            # variables for test
+            $CommandBody = '
+            . "C:\Users\circleci.$env:COMPUTERNAME\project\jumpcloud-ADMU\Powershell\Start-Migration.ps1"
+            # Trim env vars with hardcoded ""
+            $JCU = ${ENV:$JcUserName}.Trim([char]0x0022)
+            $SU = ${ENV:$SelectedUserName}.Trim([char]0x0022)
+            $PW = ${ENV:$TempPassword}.Trim([char]0x0022)
+            Start-Migration -JumpCloudUserName $JCU -SelectedUserName $ENV:COMPUTERNAME\$SU -TempPassword $PW
+            '
+            $CommandTrigger = 'ADMU'
+            $CommandName = 'RemoteADMU'
+            # clear command results
+            $results = Get-JcSdkCommandResult
+            foreach ($result in $results) {
+                # Delete Command Results
+                Write-Host "Found Command Results: $($result.id) removing..."
+                remove-jcsdkcommandresult -id $result.id
             }
-            $Form = @{
-                '$JcUserName'       = $user.JCUsername;
-                '$SelectedUserName' = $user.Username;
-                '$TempPassword'     = $user.Password
-            } | ConvertTo-Json
-            Invoke-RestMethod -Method POST -Uri "https://console.jumpcloud.com/api/command/trigger/$($CommandTrigger)" -ContentType 'application/json' -Headers $headers -Body $Form
-            Write-Host "Invoke Command ADMU:"
-            $count = 0
-            do {
-                $invokeResults = Get-JcSdkCommandResult
-                Write-Host "Waiting 5 seconds for system to receive command..."
-                $count += 1
-                start-sleep 5
-            } until (($invokeResults) -or ($count -eq 48))
-            Write-Host "Command pushed to system, waiting on results"
-            $count = 0
-            do {
-                $CommandResults = Get-JCCommandResult -CommandResultID $invokeResults.Id
-                Write-host "Waiting 5 seconds on results..."
-                $count += 1
-                start-sleep 5
-            } until ((($CommandResults.exitCode) -is [int]) -or ($count -eq 48))
-            $CommandResults.exitCode | Should -Be 0
+            # Clear previous commands matching the name
+            $RemoteADMUCommands = Get-JcSdkCommand | Where-Object { $_.name -eq $CommandName }
+            foreach ($result in $RemoteADMUCommands) {
+                # Delete Command Results
+                Write-Host "Found existing Command: $($result.id) removing..."
+                Remove-JcSdkCommand -id $result.id
+            }
+
+            # Create command & association to command
+            New-JcSdkCommand -Command $CommandBody -CommandType "windows" -Name $CommandName -Trigger $CommandTrigger -Shell powershell
+            $CommandID = (Get-JcSdkCommand | Where-Object { $_.Name -eq $CommandName }).Id
+            Write-Host "Setting CommandID: $CommandID associations"
+            Set-JcSdkCommandAssociation -CommandId $CommandID -Id $systemKey -Op add -Type system
+        }
+        It 'Test that system key exists' {
+            $systemKey | Should -Not -BeNullOrEmpty
+        }
+        It 'Invoke ADMU from JumpCloud Command' {
+            # clear results
+            $results = Get-JcSdkCommandResult
+            foreach ($result in $results) {
+                # Delete Command Results
+                remove-jcsdkcommandresult -id $result.id
+            }
+            # begin tests
+            foreach ($user in $JCCommandTestingHash.Values) {
+                write-host "Running: Start-Migration -JumpCloudUserName $($user.JCUsername) -SelectedUserName $($user.username) -TempPassword $($user.password)"
+                $headers = @{
+                    'Accept'    = "application/json"
+                    'x-api-key' = $env:JCApiKey
+                }
+                $Form = @{
+                    '$JcUserName'       = $user.JCUsername;
+                    '$SelectedUserName' = $user.Username;
+                    '$TempPassword'     = $user.Password
+                } | ConvertTo-Json
+                Invoke-RestMethod -Method POST -Uri "https://console.jumpcloud.com/api/command/trigger/$($CommandTrigger)" -ContentType 'application/json' -Headers $headers -Body $Form
+                Write-Host "Invoke Command ADMU:"
+                $count = 0
+                do {
+                    $invokeResults = Get-JcSdkCommandResult
+                    Write-Host "Waiting 5 seconds for system to receive command..."
+                    $count += 1
+                    start-sleep 5
+                } until (($invokeResults) -or ($count -eq 48))
+                Write-Host "Command pushed to system, waiting on results"
+                $count = 0
+                do {
+                    $CommandResults = Get-JCCommandResult -CommandResultID $invokeResults.Id
+                    Write-host "Waiting 5 seconds on results..."
+                    $count += 1
+                    start-sleep 5
+                } until ((($CommandResults.exitCode) -is [int]) -or ($count -eq 48))
+                $CommandResults.exitCode | Should -Be 0
+            }
         }
     }
+    AfterEach {
+        Write-Host "End Test: $testName"
+        Write-Host "---------------------------`n"
+    }
 }
+
 AfterAll {
     $systems = Get-JCsdkSystem
     $CIsystems = $systems | Where-Object { $_.displayname -match "packer" }
