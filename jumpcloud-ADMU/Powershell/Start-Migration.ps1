d61b935e3e780cca78f8e0064203aff12024c3d2
Ken Maranion <ken.maranion@jumpcloud.com>
2023-08-28 14:46:28 -0700

    Version

--- jumpcloud-ADMU/Powershell/Start-Migration.ps1
+++ jumpcloud-ADMU/Powershell/Start-Migration.ps1
@@ -1433,7 +1433,7 @@ Function Start-Migration {
     Begin {
         Write-ToLog -Message:('####################################' + (get-date -format "dd-MMM-yyyy HH:mm") + '####################################')
         # Start script
-        $admuVersion = '2.4.4'
+        $admuVersion = '2.5.0'
         Write-ToLog -Message:('Running ADMU: ' + 'v' + $admuVersion)
         Write-ToLog -Message:('Script starting; Log file location: ' + $jcAdmuLogFile)
         Write-ToLog -Message:('Gathering system & profile information')
