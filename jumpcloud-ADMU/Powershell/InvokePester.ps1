dfde3874f1aecdc3f13cd774a4786fd47e8b6528
Joe Workman <joe.workman@jumpcloud.com>
2023-10-13 08:07:15 -0600

    Admu spit tests by matrix

--- jumpcloud-ADMU/Powershell/InvokePester.ps1
+++ jumpcloud-ADMU/Powershell/InvokePester.ps1
@@ -15,13 +15,34 @@ Write-host "Running Pester Tests using Pester Version: $($PesterInstalledVersion
 # Run Pester tests
 $PesterResultsFileXmldir = "$PSScriptRoot/../test_results/"
 # $PesterResultsFileXml = $PesterResultsFileXmldir + "results.xml"
-if (-not (Test-Path $PesterResultsFileXmldir))
-{
+if (-not (Test-Path $PesterResultsFileXmldir)) {
     new-item -path $PesterResultsFileXmldir -ItemType Directory
 }
 
-$configuration = [PesterConfiguration]::Default
-$configuration.Run.Path = "$PSScriptRoot/Tests/"
+# Define CI Matrix Job Set:
+If ($CI) {
+    $jobMatrixSet = @{
+        0 = @{
+            'filePath' = @(
+                "$PSScriptRoot/Tests/Migration.Tests.ps1"
+            )
+        }
+        1 = @{
+            'filePath' = @(
+                "$PSScriptRoot/Tests/Build.Tests.ps1"
+            )
+        }
+    }
+    write-host "running CI job group: $job_group"
+    $configRunPath = $jobMatrixSet[$env:job_group].filePath
+} else {
+    configRunPath = "$PSScriptRoot/Tests/"
+}
+
+break
+
+$configuration = New-PesterConfiguration
+$configuration.Run.Path = $configRunPath
 $configuration.Should.ErrorAction = 'Continue'
 $configuration.CodeCoverage.Enabled = $true
 $configuration.testresult.Enabled = $true
@@ -32,20 +53,15 @@ $configuration.testresult.OutputPath = ($PesterResultsFileXmldir + 'results.xml'
 Invoke-Pester -configuration $configuration
 
 $PesterTestResultPath = (Get-ChildItem -Path:("$($PesterResultsFileXmldir)")).FullName | Where-Object { $_ -match "results.xml" }
-If (Test-Path -Path:($PesterTestResultPath))
-{
+If (Test-Path -Path:($PesterTestResultPath)) {
     [xml]$PesterResults = Get-Content -Path:($PesterTestResultPath)
-    If ($PesterResults.ChildNodes.failures -gt 0)
-    {
+    If ($PesterResults.ChildNodes.failures -gt 0) {
         Write-Error ("Test Failures: $($PesterResults.ChildNodes.failures)")
     }
-    If ($PesterResults.ChildNodes.errors -gt 0)
-    {
+    If ($PesterResults.ChildNodes.errors -gt 0) {
         Write-Error ("Test Errors: $($PesterResults.ChildNodes.errors)")
     }
-}
-Else
-{
+} Else {
     Write-Error ("Unable to find file path: $PesterTestResultPath")
 }
 Write-Host -ForegroundColor Green '-------------Done-------------'
