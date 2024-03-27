3b80516f2406a2b06a02de254e309a59ec5bbdba
Ken Maranion <ken.maranion@jumpcloud.com>
2024-03-27 14:36:04 -0700

    hostname and local user validation check

--- jumpcloud-ADMU/Powershell/Start-Migration.ps1
+++ jumpcloud-ADMU/Powershell/Start-Migration.ps1
@@ -1898,6 +1898,12 @@ Function Start-Migration {
             break
         }
 
+        # Get the value of selectedusername after / if it exists "domain\username" get the username
+        $selectedUsernameVal = $SelectedUserName.Split('\')[-1]
+        if($JumpCloudUserName -eq $selectedUsernameVal) {
+            Throw [System.Management.Automation.ValidationMetadataException] "JumpCloudUserName and SelectedUserName cannot be the same. Exiting..."
+            break
+        }
         # Conditional ParameterSet logic
         If ($PSCmdlet.ParameterSetName -eq "form") {
             $SelectedUserName = $inputObject.SelectedUserName
