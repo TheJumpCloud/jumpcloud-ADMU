b564bf097b288eb0bdf63793270c3a16ce571425
Joe Workman <joe.workman@jumpcloud.com>
2024-12-11 12:08:10 -0700

    combine discovery and collate scripts

---
+++ jumpcloud-ADMU-Advanced-Deployment/InvokeFromJCAgent/1_ADMU_Collect.ps1
@@ -0,0 +1,96 @@
+# Github vars
+$GHUsername = ''
+$GHToken = '' # https://github.com/settings/tokens needs token to write/create repo
+$GHRepoName = 'Jumpcloud-ADMU-Discovery'
+$password = ConvertTo-SecureString "$GHToken" -AsPlainText -Force
+$Cred = New-Object System.Management.Automation.PSCredential ($GHUsername, $password)
+
+# Install PowerShellForGitHub
+if ($null -eq (Get-InstalledModule -Name "PowerShellForGitHub" -ErrorAction SilentlyContinue)) {
+    Install-Module PowerShellForGitHub -Force
+}
+
+# If system is Mac, save to home directory
+If ($IsMacOS) {
+    $tempDir = '~/'
+    $newjsonoutputdir = $tempDir + '/' + $env:COMPUTERNAME + '.json'
+    $workingdir = $tempDir + '\jumpcloud-discovery'
+    $discoverycsvlocation = $workingdir + '\jcdiscovery.csv'
+} elseif ($IsWindows) {
+    # If system is Windows and running Powershell 7.x.xxx, save to %TEMP%\Jumpcloud-discovery
+    $windowstemp = [System.Environment]::GetEnvironmentVariable('TEMP', 'Machine')
+    $newjsonoutputdir = $windowstemp + '\' + $env:COMPUTERNAME + '.json'
+    $workingdir = $windowstemp + '\jumpcloud-discovery'
+    $discoverycsvlocation = $workingdir + '\jcdiscovery.csv'
+} else {
+    # Assume PowerShell 5.1.xxx
+    $windowstemp = [System.Environment]::GetEnvironmentVariable('TEMP', 'Machine')
+    $newjsonoutputdir = $windowstemp + '\' + $env:COMPUTERNAME + '.json'
+    $workingdir = $windowstemp + '\jumpcloud-discovery'
+    $discoverycsvlocation = $workingdir + '\jcdiscovery.csv'
+}
+
+
+function Get-ADMUSystemsForMigration {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [system.string]
+        $systemID
+    )
+    begin {
+        If ('systemID' -in $PSBoundParameters) {
+            $systems = Get-JCSystem -SystemID $systemID
+        } else {
+            $systems = Get-JCSystem -os windows
+        }
+        # if ($systemID) {
+        # } else {
+        # $systems = Get-JCSystem -os "windows"
+        # }
+        $list = New-Object System.Collections.ArrayList
+
+    }
+    process {
+        foreach ($system in $systems) {
+            # if ($system.domainInfo.partOfDomain) {
+            $users = Get-JCsdkSystemInsightUser -Filter @("system_id:eq:$($system.id)")
+            # get the administrator account:
+            $adminUser = $users | Where-Object { $_.uid -eq 500 }
+            $machineSID = ($adminUser.uuid -split "-")[0..6] -join "-"
+
+            $adUsers = $users | Where-Object { ($_.uuid -notmatch $machineSID) -AND ($_.RealUser -eq $true) }
+            $adUsers | ForEach-Object {
+                $list.Add(
+                    [PSCustomObject]@{
+                        SID               = $_.Uuid
+                        LocalPath         = $_.Directory
+                        LocalComputerName = $system.hostname
+                        LocalUsername     = if (-NOT [system.string]::IsNullOrEmpty($_.Username)) { $_.Username } else {
+                            $_.Uuid
+                        }
+                        JumpCloudUserName = $null
+                    }
+                ) | Out-Null
+            }
+            # }
+
+        }
+    }
+    end {
+        return $list
+    }
+}
+$allUsers = Get-ADMUSystemsForMigration
+if (-NOT (Test-Path -Path $workingdir)) {
+    New-Item -ItemType Directory -Force -Path $workingdir | Out-Null
+}
+$combinedjson = $AllUsers | ConvertTo-Csv -NoTypeInformation | Out-File $discoverycsvlocation
+# Auth to github
+Set-GitHubAuthentication -Credential $cred
+
+# set content string
+$discoverycsvContent = (get-content -Path $discoverycsvlocation -Raw)
+
+# upload to github
+Set-GitHubContent -OwnerName $GHUsername -RepositoryName $GHRepoName -BranchName 'main' -Path "jcdiscovery.csv" -CommitMessage "CSV Upload $(Get-Date)" -Content $discoverycsvContent
--- jumpcloud-ADMU-Advanced-Deployment/ReadMe.md
+++ jumpcloud-ADMU-Advanced-Deployment/ReadMe.md
@@ -1,8 +1,6 @@
-# jumpcloud-ADMU Advanced Deployment:
+# Remotely migrating AD users with the ADMU
 
-Files:
-- invoke-admu-discovery.ps1
-- invoke-admu-migration.ps1
+It's possible to remotely migrate users with the ADMU PowerShell Module, in order to do this, devices will need some agent or credential set to execute code remotely. The JumpCloud ADMU is distributed as both a GUI exe and a PowerShell Module. Remote devices can call the function `Start-Migration` when the [JumpCloud.ADMU PowerShell module](https://www.powershellgallery.com/packages/JumpCloud.ADMU) is installed and migrate users from a single command. Installing the JumpCloud is one such way to remotely execute code on devices. With the JumpCloud agent installed remote commands can be executed on devices and the ADMU module can migrate individual users through a command template.
 
 Explanation:
 See https://github.com/TheJumpCloud/jumpcloud-ADMU/wiki/advanced-deployment-scenarios
