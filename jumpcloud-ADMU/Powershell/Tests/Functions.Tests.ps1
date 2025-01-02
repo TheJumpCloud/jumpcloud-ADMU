7bf88b9396a1b2f36070c78546c4e807797b3c25
Ken Maranion <ken.maranion@jumpcloud.com>
2025-01-02 10:32:51 -0800

    functions test

--- jumpcloud-ADMU/Powershell/Tests/Functions.Tests.ps1
+++ jumpcloud-ADMU/Powershell/Tests/Functions.Tests.ps1
@@ -709,4 +709,30 @@ Describe 'Functions' {
             }
         }
     }
+
+    # Test for Test-UserFolderRedirect
+    Context 'Validates that the User shell folder for default values' {
+        BeforeAll {
+            $currentUser = $(whoami) -replace "$(hostname)\\", ("")
+            $currentSID = Get-SID -User:($currentUser)
+            $currentSID | Should -Match "^S-\d-\d+-(\d+-){1,14}\d+$"
+        }
+        # Test for Test-UserFolderRedirect should be default values
+        It 'Test-UserFolderRedirect - Default values' {
+            $folderRedirect = Test-UserFolderRedirect -SID $currentSID
+            $folderRedirect | Should -Be $true
+        }
+        # Test for Test-UserFolderRedirect with one of the folder redirect values changed
+        It 'Test-UserFolderRedirect - One value changed' {
+            # Change the value of the folder Desktop to a different value
+            Set-ValueToKey -registryRoot LocalMachine -keyPath "HKEY_USERS:$($currentSID)\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" -name "Desktop" -value "\\server\share\desktop"
+            $folderRedirect = Test-UserFolderRedirect -SID $currentSID
+            $folderRedirect | Should -Be $false
+        }
+        # Test for Invalid SID or Invalid User Shell Folder
+        It 'Test-UserFolderRedirect - Invalid SID or Invalid User Shell Folder' {
+            $folderRedirect = Test-UserFolderRedirect -SID "Invalid-3361044348-30300820-1001"
+            $folderRedirect | Should -Be $false
+        }
+    }
 }
