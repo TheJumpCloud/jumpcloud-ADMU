68231914714c25981539f551e3415e3a88b634fb
Joe Workman <joe.workman@jumpcloud.com>
2025-02-07 12:04:37 -0700

    Get-AppxList acceptance tests

---
+++ jumpcloud-ADMU/Powershell/Tests/Private/appxPackages/Get-AppxListByUser.Acceptance.Tests.ps1
@@ -0,0 +1,42 @@
+Describe "Get-AppxListByUser Acceptance Tests" {
+    BeforeAll {
+        # import all functions
+        $currentPath = $PSScriptRoot # Start from the current script's directory.
+        $TargetDirectory = "helperFunctions"
+        $FileName = "Import-AllFunctions.ps1"
+        while ($currentPath -ne $null) {
+            $filePath = Join-Path -Path $currentPath $TargetDirectory $FileName
+            if (Test-Path $filePath) {
+                # File found! Return the full path.
+                $helpFunctionDir = $filePath
+                break
+            }
+
+            # Move one directory up.
+            $currentPath = Split-Path $currentPath -Parent
+        }
+        . "$helpFunctionDir"
+    }
+    Context "Successful conditions" {
+        It "Valid SID, Secure Channel Established" {
+            $currentUserSID = (Get-LocalUser -Name $env:USERNAME | Select-Object SID).SID
+            write-host "SID $currentUserSID"
+            $appxList = Get-AppxListByUser -SID $currentUserSID
+            # install location items should be populated
+            $appxList.InstallLocation | Should -Not -BeNullOrEmpty
+        }
+        It "Valid SID, Secure Channel Not Established" {
+            # mock the secure channel returning false
+            Mock Test-ComputerSecureChannel { return $false }
+            $currentUserSID = (Get-LocalUser -Name $env:USERNAME | Select-Object SID).SID
+            $appxList = Get-AppxListByUser -SID $currentUserSID
+            # install location items should be populated
+            $appxList.InstallLocation | Should -Not -BeNullOrEmpty
+        }
+    }
+    Context "Error handling" {
+        It "Invalid SID" {
+            { Get-AppxListByUser -SID "abcd" }  | Should -Throw
+        }
+    }
+}
