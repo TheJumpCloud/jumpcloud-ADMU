282762d1ae604d6b0a11fc43ab479004486bbb8f
Joe Workman <joe.workman@jumpcloud.com>
2025-02-10 14:07:52 -0700

    progress form tests

---
+++ jumpcloud-ADMU/Powershell/Tests/Private/DisplayForms/ProgressForm.Acceptance.Tests.ps1
@@ -0,0 +1,26 @@
+Describe "ProgressForm Acceptance Tests" {
+    BeforeAll {
+        # import all functions
+        $currentPath = $PSScriptRoot # Start from the current script's directory.
+        $TargetDirectory = "helperFunctions"
+        $FileName = "Import-AllFunctions.ps1"
+        while ($currentPath -ne $null) {
+            $filePath = Join-Path -Path $currentPath $TargetDirectory
+            if (Test-Path $filePath) {
+                # File found! Return the full path.
+                $helpFunctionDir = $filePath
+                break
+            }
+
+            # Move one directory up.
+            $currentPath = Split-Path $currentPath -Parent
+        }
+        . "$helpFunctionDir\$fileName"
+    }
+    It "Should load the form without throwing" {
+        # Add acceptance test logic and assertions (against a real system)
+        { New-ProgressForm } | Should -Not -Throw
+    }
+
+    # Add more acceptance tests as needed
+}
