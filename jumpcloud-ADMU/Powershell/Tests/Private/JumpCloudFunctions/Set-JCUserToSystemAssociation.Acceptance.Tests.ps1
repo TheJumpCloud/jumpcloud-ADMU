869a77182ef78eab6356b3ec6db7e37af29250d7
Joe Workman <joe.workman@jumpcloud.com>
2025-02-11 15:15:09 -0700

    temp path for download

--- jumpcloud-ADMU/Powershell/Tests/Private/JumpCloudFunctions/Set-JCUserToSystemAssociation.Acceptance.Tests.ps1
+++ jumpcloud-ADMU/Powershell/Tests/Private/JumpCloudFunctions/Set-JCUserToSystemAssociation.Acceptance.Tests.ps1
@@ -24,7 +24,7 @@ Describe "Set-JCUserToSystemAssociation Acceptance Tests" -Tag "Acceptance", "In
             $AGENT_INSTALLER_URL = "https://cdn02.jumpcloud.com/production/jcagent-msi-signed.msi"
             $AGENT_PATH = Join-Path ${env:ProgramFiles} "JumpCloud"
             $AGENT_CONF_PATH = "$($AGENT_PATH)\Plugins\Contrib\jcagent.conf"
-            $AGENT_INSTALLER_PATH = "C:\Windows\Temp\JCADMU\jcagent-msi-signed.msi"
+            $AGENT_INSTALLER_PATH = "C:\Windows\Temp\jcagent-msi-signed.msi"
             $AGENT_BINARY_NAME = "jumpcloud-agent.exe"
             $CONNECT_KEY = $env:PESTER_CONNECTKEY
 
