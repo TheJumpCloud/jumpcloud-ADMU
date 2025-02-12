4412944f32366d94b74936b73b871ee3a37cf547
Ken Maranion <ken.maranion@jumpcloud.com>
2025-02-12 11:21:39 -0800

    config output

--- jumpcloud-ADMU/Powershell/InvokePester.ps1
+++ jumpcloud-ADMU/Powershell/InvokePester.ps1
@@ -103,7 +103,8 @@ $configuration.CodeCoverage.OutputPath = ($PesterResultsFileXmlDir + 'coverage.x
 $configuration.testResult.OutputPath = ($PesterResultsFileXmlDir + 'results.xml')
 Write-Host ("[RUN COMMAND] Invoke-Pester -Path:('$PesterRunPath') -TagFilter:('$($IncludeTags -join "','")') -ExcludeTagFilter:('$($ExcludeTagList -join "','")') -PassThru") -BackgroundColor:('Black') -ForegroundColor:('Magenta')
 
-Write-Host "-------------------------- $Configuration"
+# Output the Configureation object to the screen
+$configuration | Format-List | Out-String | Write-Host -BackgroundColor:('Black') -ForegroundColor:('Cyan')
 Invoke-Pester -configuration $configuration
 
 $PesterTestResultPath = (Get-ChildItem -Path:("$($PesterResultsFileXmlDir)")).FullName | Where-Object { $_ -match "results.xml" }
