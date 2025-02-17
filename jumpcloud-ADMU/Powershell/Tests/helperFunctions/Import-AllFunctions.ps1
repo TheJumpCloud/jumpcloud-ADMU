fa95583ffef9e3206cc1ba55176af244243d22e3
Joe Workman <joe.workman@jumpcloud.com>
2025-02-17 12:38:32 -0700

    import all functions path

--- jumpcloud-ADMU/Powershell/Tests/helperFunctions/Import-AllFunctions.ps1
+++ jumpcloud-ADMU/Powershell/Tests/helperFunctions/Import-AllFunctions.ps1
@@ -1,11 +1,10 @@
 
 # Get the module root path
 $currentTestPath = $PSScriptRoot
-$regexPattern = "\\jumpcloud-ADMU\\jumpcloud-ADMU\\.*|\/jumpcloud-ADMU/jumpcloud-ADMU\/.*"
-$Global:rootModule = $currentTestPath -replace $regexPattern, "\jumpcloud-ADMU"
+$Global:rootModule = "$currentTestPath\..\..\..\..\"
 
 # Import Private Functions:
-$Private = @( Get-ChildItem -Path "$Global:rootModule/jumpcloud-ADMU/Powershell/Private/*.ps1" -Recurse)
+$Private = @( Get-ChildItem -Path "$Global:rootModule\jumpcloud-ADMU\Powershell/Private/*.ps1" -Recurse)
 Foreach ($Import in $Private) {
     Try {
         # Write-Output "Importing Private Function: $($Import.BaseName) "
@@ -15,7 +14,7 @@ Foreach ($Import in $Private) {
     }
 }
 # Import Public Functions:
-$Private = @( Get-ChildItem -Path "$Global:rootModule/jumpcloud-ADMU/Powershell/Public/*.ps1" -Recurse)
+$Private = @( Get-ChildItem -Path "$Global:rootModule\jumpcloud-ADMU\Powershell/Public/*.ps1" -Recurse)
 Foreach ($Import in $Private) {
     Try {
         # Write-Output "Importing Public Function: $($Import.BaseName) "
