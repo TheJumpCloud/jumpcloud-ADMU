3ba239e12e6331ecc6c2488c051fe7e2e20f2e8b
Ken Maranion <97972790+kmaranionjc@users.noreply.github.com>
2025-03-18 13:51:36 -0700

    Folder Redirection Enhancement (#162) (#163)
    * Folder Redirection Enhancement (#162)
    
    * folder redirect enhancement
    
    * date
    
    * changelog date + comment
    
    * psd1
    
    * psd1 test
    
    * psd1
    
    * test psd1
    
    * version check for PSD1
    
    ---------
    
    Co-authored-by: Joe Workman <joe.workman@jumpcloud.com>

--- jumpcloud-ADMU/Powershell/Tests/Private/UserIdentification/Test-UserFolderRedirect.Acceptance.Tests.ps1
+++ jumpcloud-ADMU/Powershell/Tests/Private/UserIdentification/Test-UserFolderRedirect.Acceptance.Tests.ps1
@@ -41,6 +41,7 @@ Describe "Test-UserFolderRedirect Acceptance Tests" -Tag "Acceptance" {
             $userSid = Test-UsernameOrSID -usernameOrSid $newUser
             # Load the registry hive for the user and add _admu after the sid
             REG LOAD HKU\$($userSid)_admu "C:\Users\$newUser\NTUSER.DAT" *>&1
+            $folderPath = "HKEY_USERS:\$($userSid)_admu\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders"
         }
         # Test for Test-UserFolderRedirect should be default values
         It 'Test-UserFolderRedirect - Default values' {
@@ -50,16 +51,72 @@ Describe "Test-UserFolderRedirect Acceptance Tests" -Tag "Acceptance" {
         # Test for Test-UserFolderRedirect with one of the folder redirect values changed
         It 'Test-UserFolderRedirect - One value changed' {
             # Change the value of the folder Desktop to a different value
-            $folderPath = "HKEY_USERS:\$($userSid)_admu\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders"
+            Write-Output "Changing the value of the folder Desktop to a different value $($folderPath)"
             Set-ItemProperty -Path $folderpath -Name Desktop -Value "\\server\share\desktop"
+            # Validate that the folder has changed
+            (Get-ItemProperty -Path $folderpath).Desktop | Should -Be "\\server\share\desktop"
             Test-UserFolderRedirect -UserSid $userSid | Should -Be $true
             # Change the value of the folder Desktop back to the default value
             Set-ItemProperty -Path $folderpath -Name Desktop -Value "%USERPROFILE%\Desktop"
-
         }
         # Test for Invalid SID or Invalid User Shell Folder
         It 'Test-UserFolderRedirect - Invalid SID or Invalid User Shell Folder' {
             Test-UserFolderRedirect -UserSid "Invalid-3361044348-30300820-1001" | Should -be $true
+        }
+        # Test for IP Path
+        It 'Test-UserFolderRedirect - IP Path' {
+            # Change the value of the folder Documents to an IP path
+            Set-ItemProperty -Path $folderpath -Name "My Music" -Value "\\192.168.1.10\SharedFolder\Music"
+            # Validate that the folder has changed
+            (Get-ItemProperty -Path $folderpath)."My Music" | Should -Be "\\192.168.1.10\SharedFolder\Music"
+            # Test path
+            Write-Host $userSid
+            Test-UserFolderRedirect -UserSid $userSid | Should -Be $true
+            # Change the value of the folder Documents back to the default value
+            Set-ItemProperty -Path $folderpath -Name "My Music" -Value "%USERPROFILE%\Music"
+        }
+        # Test for Domain Path
+        It 'Test-UserFolderRedirect - Domain Path' {
+            # Change the value of the folder Documents to a domain path
+            Set-ItemProperty -Path $folderpath -Name Desktop -Value "\\domain.local\SharedFolder\Desktop"
+            # Validate that the folder has changed
+            (Get-ItemProperty -Path $folderpath).Desktop | Should -Be "\\domain.local\SharedFolder\Desktop"
+            Test-UserFolderRedirect -UserSid $userSid | Should -Be $true
+            # Change the value of the folder Desktop back to the default value
+            Set-ItemProperty -Path $folderpath -Name Desktop -Value "%USERPROFILE%\Desktop"
+        }
+        It 'Test-UserFolderRedirect - My Drive (GDrive)' {
+            # Change the value of the folder Documents to a Google Drive path/different drive letter
+            # Note: This is a test for a Google Drive path, but it can be adapted for any other drive letter
+            Set-ItemProperty -Path $folderpath -Name "{374DE290-123F-4565-9164-39C4925E467B}" -Value "G:\My Drive\Downloads" # Using the Downloads ID
+            # Validate that the folder has changed
+            (Get-ItemProperty -Path $folderpath)."{374DE290-123F-4565-9164-39C4925E467B}" | Should -Be "G:\My Drive\Downloads"
+            # Test path
+            Test-UserFolderRedirect -UserSid $userSid | Should -Be $false
+
+            Set-ItemProperty -Path $folderpath -Name "{374DE290-123F-4565-9164-39C4925E467B}" -Value "%USERPROFILE%\Downloads"
+        }
+        # Test for OneDrive in the path
+        It 'Test-UserFolderRedirect - OneDrive' {
+            # Change the value of the folder Documents to a OneDrive path
+            Set-ItemProperty -Path $folderpath -Name Documents -Value "C:\Users\$newUser\OneDrive\Documents"
+            # Validate that the folder has changed
+            (Get-ItemProperty -Path $folderpath).Documents | Should -Be "C:\Users\$newUser\OneDrive\Documents"
+
+            # Test path
+            Test-UserFolderRedirect -UserSid $userSid | Should -Be $false
+
+            # Set the folder values back to default
+            Set-ItemProperty -Path $folderpath -Name Documents -Value "%USERPROFILE%\Documents"
+        }
+        # Test for a default value for the folder path
+        It 'Test-UserFolderRedirect - Default value for the folder path' {
+            # Change the value of the folder Documents to a default value
+            Set-ItemProperty -Path $folderpath -Name Documents -Value "%USERPROFILE%\Documents"
+            # Validate that the folder has changed
+            (Get-ItemProperty -Path $folderpath).Documents | Should -Be "%USERPROFILE%\Documents"
+            # Test path
+            Test-UserFolderRedirect -UserSid $userSid | Should -Be $false
         }
     }
 }
