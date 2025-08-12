31162d11c1c0a0735f5491e7f09b38974e347582
Ken Maranion <ken.maranion@jumpcloud.com>
2025-08-12 11:17:53 -0700

    test

--- jumpcloud-ADMU/Powershell/Tests/Public/invokeMigration.Tests.ps1
+++ jumpcloud-ADMU/Powershell/Tests/Public/invokeMigration.Tests.ps1
@@ -44,7 +44,7 @@ Describe "ADMU Bulk Migration Script CI Tests" -Tag "Migration Parameters" {
 
             # Act & Assert: The script should throw a specific error that identifies
 
-            { & $global:scriptToTest } | Should -Throw "VALIDATION FAILED: Duplicate SID 'S-1-5-21-DUPLICATE-SID' found for LocalComputerName 'TEST-PC-1'. SIDs must be unique per device. Halting script."
+            { & $global:scriptToTest } | Should -Throw "Error importing CSV file, [error] Duplicate SID 'S-1-5-21-DUPLICATE-SID' found for LocalComputerName 'TEST-PC-1'. SIDs must be unique per device."
         }
 
         It "Should throw an error if 'LocalPath' is empty" {
@@ -105,76 +105,74 @@ Describe "ADMU Bulk Migration Script CI Tests" -Tag "Migration Parameters" {
         }
     }
 
-    Context "Test Migration using Invoke" {
-        BeforeAll {
-            # for these tests, the jumpCloud agent needs to be installed:
-            $AgentService = Get-Service -Name "jumpcloud-agent" -ErrorAction SilentlyContinue
-            If (-Not $AgentService) {
-                # set install variables
-                $AGENT_INSTALLER_URL = "https://cdn02.jumpcloud.com/production/jcagent-msi-signed.msi"
-                $AGENT_PATH = Join-Path ${env:ProgramFiles} "JumpCloud"
-                $AGENT_CONF_PATH = "$($AGENT_PATH)\Plugins\Contrib\jcagent.conf"
-                $AGENT_INSTALLER_PATH = "C:\Windows\Temp\jcagent-msi-signed.msi"
-                $AGENT_BINARY_NAME = "jumpcloud-agent.exe"
-                $CONNECT_KEY = $env:PESTER_CONNECTKEY
+    #     Context "Test Migration using Invoke" {
+    #         BeforeAll {
+    #             # for these tests, the jumpCloud agent needs to be installed:
+    #             $AgentService = Get-Service -Name "jumpcloud-agent" -ErrorAction SilentlyContinue
+    #             If (-Not $AgentService) {
+    #                 # set install variables
+    #                 $AGENT_INSTALLER_URL = "https://cdn02.jumpcloud.com/production/jcagent-msi-signed.msi"
+    #                 $AGENT_PATH = Join-Path ${env:ProgramFiles} "JumpCloud"
+    #                 $AGENT_CONF_PATH = "$($AGENT_PATH)\Plugins\Contrib\jcagent.conf"
+    #                 $AGENT_INSTALLER_PATH = "C:\Windows\Temp\jcagent-msi-signed.msi"
+    #                 $AGENT_BINARY_NAME = "jumpcloud-agent.exe"
+    #                 $CONNECT_KEY = $env:PESTER_CONNECTKEY
 
-                # now go install the agent
-                Install-JumpCloudAgent -AGENT_INSTALLER_URL:($AGENT_INSTALLER_URL) -AGENT_INSTALLER_PATH:($AGENT_INSTALLER_PATH) -AGENT_CONF_PATH:($AGENT_CONF_PATH) -JumpCloudConnectKey:($CONNECT_KEY) -AGENT_PATH:($AGENT_PATH) -AGENT_BINARY_NAME:($AGENT_BINARY_NAME)
-            }
+    #                 # now go install the agent
+    #                 Install-JumpCloudAgent -AGENT_INSTALLER_URL:($AGENT_INSTALLER_URL) -AGENT_INSTALLER_PATH:($AGENT_INSTALLER_PATH) -AGENT_CONF_PATH:($AGENT_CONF_PATH) -JumpCloudConnectKey:($CONNECT_KEY) -AGENT_PATH:($AGENT_PATH) -AGENT_BINARY_NAME:($AGENT_BINARY_NAME)
+    #             }
 
-            # Auth to the JumpCloud Module
-            Connect-JCOnline -JumpCloudApiKey $env:PESTER_APIKEY -JumpCloudOrgId $env:PESTER_ORGID -Force
+    #             # Auth to the JumpCloud Module
+    #             Connect-JCOnline -JumpCloudApiKey $env:PESTER_APIKEY -JumpCloudOrgId $env:PESTER_ORGID -Force
 
-            # get the org details
-            $OrgSelection, $MTPAdmin = Get-MtpOrganization -apiKey $env:PESTER_APIKEY
-            $OrgName = "$($OrgSelection[1])"
-            $OrgID = "$($OrgSelection[0])"
-            # get the system key
-            $config = get-content "C:\Program Files\JumpCloud\Plugins\Contrib\jcagent.conf"
-            $regex = 'systemKey\":\"(\w+)\"'
-            $systemKey = [regex]::Match($config, $regex).Groups[1].Value
-            $global:scriptToTest = Join-Path $PSScriptRoot '..\..\..\..\jumpcloud-ADMU-Advanced-Deployment\InvokeFromJCAgent\3_ADMU_Invoke.ps1'
+    #             # get the org details
+    #             $OrgSelection, $MTPAdmin = Get-MtpOrganization -apiKey $env:PESTER_APIKEY
+    #             $OrgName = "$($OrgSelection[1])"
+    #             $OrgID = "$($OrgSelection[0])"
+    #             # get the system key
+    #             $config = get-content "C:\Program Files\JumpCloud\Plugins\Contrib\jcagent.conf"
+    #             $regex = 'systemKey\":\"(\w+)\"'
+    #             $systemKey = [regex]::Match($config, $regex).Groups[1].Value
+    #             $global:scriptToTest = Join-Path $PSScriptRoot '..\..\..\..\jumpcloud-ADMU-Advanced-Deployment\InvokeFromJCAgent\3_ADMU_Invoke.ps1'
 
-            if (-not (Test-Path $global:scriptToTest)) {
-                throw "TEST SETUP FAILED: Script not found at the calculated path: $($global:scriptToTest). Please check the relative path in the BeforeAll block."
-            }
-        }
-        BeforeEach {
-            # # sample password
-            $tempPassword = "Temp123!"
-            # username to migrate
-            $userToMigrateFrom = "ADMU_" + -join ((65..90) + (97..122) | Get-Random -Count 5 | ForEach-Object { [char]$_ })
-            # username to migrate to
-            $userToMigrateTo = "ADMU_" + -join ((65..90) + (97..122) | Get-Random -Count 5 | ForEach-Object { [char]$_ })
+    #             if (-not (Test-Path $global:scriptToTest)) {
+    #                 throw "TEST SETUP FAILED: Script not found at the calculated path: $($global:scriptToTest). Please check the relative path in the BeforeAll block."
+    #             }
+    #         }
+    #         BeforeEach {
+    #             # # sample password
+    #             $tempPassword = "Temp123!"
+    #             # username to migrate
+    #             $userToMigrateFrom = "ADMU_" + -join ((65..90) + (97..122) | Get-Random -Count 5 | ForEach-Object { [char]$_ })
+    #             # username to migrate to
+    #             $userToMigrateTo = "ADMU_" + -join ((65..90) + (97..122) | Get-Random -Count 5 | ForEach-Object { [char]$_ })
 
-            # Initialize-TestUser
-            Initialize-TestUser -username $userToMigrateFrom -password $tempPassword
+    #             # Initialize-TestUser
+    #             Initialize-TestUser -username $userToMigrateFrom -password $tempPassword
 
-        }
-        It "Should successfully migrate a user with valid data" {
-            # Now create a CSV to get these values: "SID","LocalPath","LocalComputerName","LocalUsername","JumpCloudUserName","JumpCloudUserID","JumpCloudSystemID","SerialNumber"
-            $userObject = New-Object System.Security.Principal.NTAccount($userToMigrateFrom)
-            $userProfile = Get-CimInstance -ClassName Win32_UserProfile | Where-Object { $_.SID -eq $sid }
+    #         }
+    #         It "Should successfully migrate a user with valid data" {
+    #             # Now create a CSV to get these values: "SID","LocalPath","LocalComputerName","LocalUsername","JumpCloudUserName","JumpCloudUserID","JumpCloudSystemID","SerialNumber"
+    #             $userObject = New-Object System.Security.Principal.NTAccount($userToMigrateFrom)
+    #             $userProfile = Get-CimInstance -ClassName Win32_UserProfile | Where-Object { $_.SID -eq $sid }
 
-            $UserSid = $userObject.Translate([System.Security.Principal.SecurityIdentifier]).Value
-            $localPath = $userProfile.LocalPath
-            $serialNumber = Get-CimInstance -ClassName Win32_BIOS | Select-Object -ExpandProperty SerialNumber
-            $csvContent = @"
-    SID,LocalPath,LocalComputerName,LocalUsername,JumpCloudUserName,JumpCloudUserID,JumpCloudSystemID,SerialNumber
-    $($userToMigrateFrom),$($localPath),$($env:COMPUTERNAME),$($userToMigrateFrom),$($userToMigrateTo),$($null),$($null),$($serialNumber)
-"@
-            $global:tempCsvPath = Join-Path 'C:\Windows\Temp' 'jcDiscovery.csv'
-            Set-Content -Path $global:tempCsvPath -Value $csvContent
+    #             $UserSid = $userObject.Translate([System.Security.Principal.SecurityIdentifier]).Value
+    #             $localPath = $userProfile.LocalPath
+    #             $serialNumber = Get-CimInstance -ClassName Win32_BIOS | Select-Object -ExpandProperty SerialNumber
+    #             $csvContent = @"
+    #     SID,LocalPath,LocalComputerName,LocalUsername,JumpCloudUserName,JumpCloudUserID,JumpCloudSystemID,SerialNumber
+    #     $($userToMigrateFrom),$($localPath),$($env:COMPUTERNAME),$($userToMigrateFrom),$($userToMigrateTo),$($null),$($null),$($serialNumber)
+    # "@
+    #             $global:tempCsvPath = Join-Path 'C:\Windows\Temp' 'jcDiscovery.csv'
+    #             Set-Content -Path $global:tempCsvPath -Value $csvContent
 
-            # Now edit the API key and orgId in the 3_ADMU_Invoke.ps1
-            (Get-Content -Path $global:scriptToTest) -replace 'JumpCloudAPIKey\s*=\s*".*?"', "JumpCloudAPIKey = `"$($env:PESTER_APIKEY)`""
-            (Get-Content -Path $global:scriptToTest) -replace 'JumpCloudOrgID\s*=\s*".*?"', "JumpCloudOrgID = `"$($env:PESTER_ORGID)`""
+    #             # Now edit the API key and orgId in the 3_ADMU_Invoke.ps
 
-            # Set the $systemContextBinding to $false
-            (Get-Content -Path $global:scriptToTest) -replace 'systemContextBinding\s*=\s*".*?"', "systemContextBinding = `"$false`""
 
-            # Run the script
-            { & $global:scriptToTest } | Should -Not -Throw
-        }
-    }
+
+    #             # Run the script
+    #             { & $global:scriptToTest } | Should -Not -Throw
+    #         }
+
+    #     }
 }
