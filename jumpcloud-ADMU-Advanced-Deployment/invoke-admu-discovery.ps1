575e7792b2474b925512128275f7fc9a9f15832c
Joe Workman <joe.workman@jumpcloud.com>
2025-10-10 09:48:18 -0600

    Get-WMIObject -> Get-CimInstance

--- jumpcloud-ADMU-Advanced-Deployment/invoke-admu-discovery.ps1
+++ jumpcloud-ADMU-Advanced-Deployment/invoke-admu-discovery.ps1
@@ -19,7 +19,7 @@ New-Item -ItemType file -path $CSV -force | Out-Null
 #Change this AD query to return the computers required below is an example
 #Check for computers logged into AD in the last 30days, search whole AD org, exclude if in a specific OU
 $Time = (Get-Date).AddDays(-30)
-$ComputerList = GET-ADComputer -filter {lastlogon -gt $Time} -searchbase 'DC=sajumpcloud,DC=com' -resultPageSize 2000 -ResultSetSize $null | Where-Object { ($_.distinguishedName -notlike "*OU=Cleanup,*") }
+$ComputerList = GET-ADComputer -filter { lastlogon -gt $Time } -searchbase 'DC=sajumpcloud,DC=com' -resultPageSize 2000 -ResultSetSize $null | Where-Object { ($_.distinguishedName -notlike "*OU=Cleanup,*") }
 $Computers = @()
 ForEach ($member in $ComputerList) {
     $name = $($member.DistinguishedName).Substring(0, $($member.DistinguishedName).IndexOf(','))
@@ -28,46 +28,50 @@ ForEach ($member in $ComputerList) {
 }
 
 #check network connectivity to computers
-$ConnectionTest = $Computers  | ForEach-Object { Test-Connection -ComputerName $_ -Count 1 -AsJob } | Get-Job | Receive-Job -Wait | Select-Object @{Name='ComputerName';Expression={$_.Address}},@{Name='Reachable';Expression={if ($_.StatusCode -eq 0) { $true } else { $false }}}
+$ConnectionTest = $Computers  | ForEach-Object { Test-Connection -ComputerName $_ -Count 1 -AsJob } | Get-Job | Receive-Job -Wait | Select-Object @{Name = 'ComputerName'; Expression = { $_.Address } }, @{Name = 'Reachable'; Expression = { if ($_.StatusCode -eq 0) { $true } else { $false } } }
 $OnlineComputers = $ConnectionTest | ? { $_.Reachable -eq $True }
 $OfflineComputers = $ConnectionTest | Where-Object { $_.Reachable -eq $False }
 
 #scriptblock to run on computers
-$ScriptBlock ={ 
-        #Get Computer Info
-        $info = Get-ComputerInfo
+$ScriptBlock = {
+    #Get Computer Info
+    $info = Get-ComputerInfo
+    try {
         $Win32UserProfiles = Get-WmiObject -Class:('Win32_UserProfile') -Property * | Where-Object { $_.Special -eq $false }
-        $Win32UserProfiles | Add-Member -MemberType NoteProperty -Name ComputerName -Value "$($info.csname)"
-        $Win32UserProfiles | Add-Member -MemberType NoteProperty -Name LocalProfileSize -Value $null
-        $Win32UserProfiles | Add-Member -MemberType NoteProperty -Name JumpCloudUserName -Value $null
-        $Win32UserProfiles | Add-Member -MemberType NoteProperty -Name SelectedUserName -Value $null
-        $Win32UserProfiles | Add-Member -MemberType NoteProperty -Name TempPassword -Value 'Temp123!'
-        $Win32UserProfiles | Add-Member -MemberType NoteProperty -Name LeaveDomain -Value 'false'
-        $Win32UserProfiles | Add-Member -MemberType NoteProperty -Name ForceReboot -Value 'false'
-        $Win32UserProfiles | Add-Member -MemberType NoteProperty -Name AzureADProfile -Value 'false'
-        $Win32UserProfiles | Add-Member -MemberType NoteProperty -Name InstallJCAgent -Value 'false'
-        $Win32UserProfiles | Add-Member -MemberType NoteProperty -Name JumpCloudConnectKey -Value '1111111111111111111111111111111111111111'
-        $Win32UserProfiles | Add-Member -MemberType NoteProperty -Name ConvertProfile -Value 'true'
-        $Win32UserProfiles | Add-Member -MemberType NoteProperty -Name MigrationSuccess -Value $null
-        $Win32UserProfiles | Add-Member -MemberType NoteProperty -Name DomainName -Value "$($info.csdomain)"
+    } catch {
+        $Win32UserProfiles = Get-CimInstance -Class:('Win32_UserProfile') -Property * | Where-Object { $_.Special -eq $false }
+    }
+    $Win32UserProfiles | Add-Member -MemberType NoteProperty -Name ComputerName -Value "$($info.csname)"
+    $Win32UserProfiles | Add-Member -MemberType NoteProperty -Name LocalProfileSize -Value $null
+    $Win32UserProfiles | Add-Member -MemberType NoteProperty -Name JumpCloudUserName -Value $null
+    $Win32UserProfiles | Add-Member -MemberType NoteProperty -Name SelectedUserName -Value $null
+    $Win32UserProfiles | Add-Member -MemberType NoteProperty -Name TempPassword -Value 'Temp123!'
+    $Win32UserProfiles | Add-Member -MemberType NoteProperty -Name LeaveDomain -Value 'false'
+    $Win32UserProfiles | Add-Member -MemberType NoteProperty -Name ForceReboot -Value 'false'
+    $Win32UserProfiles | Add-Member -MemberType NoteProperty -Name AzureADProfile -Value 'false'
+    $Win32UserProfiles | Add-Member -MemberType NoteProperty -Name InstallJCAgent -Value 'false'
+    $Win32UserProfiles | Add-Member -MemberType NoteProperty -Name JumpCloudConnectKey -Value '1111111111111111111111111111111111111111'
+    $Win32UserProfiles | Add-Member -MemberType NoteProperty -Name ConvertProfile -Value 'true'
+    $Win32UserProfiles | Add-Member -MemberType NoteProperty -Name MigrationSuccess -Value $null
+    $Win32UserProfiles | Add-Member -MemberType NoteProperty -Name DomainName -Value "$($info.csdomain)"
 
-        # #Uncomment to check profile sizes (this will add addtional time)
-        # #calculate estimated profile size
-        # $LocalUserProfiles = $Win32UserProfiles | Select-Object LocalPath
-        # $LocalUserProfilesTrim = ForEach ($LocalPath in $LocalUserProfiles) { $LocalPath.LocalPath.substring(9) }
-        # $i = 0
-        # foreach ($userprofile in $LocalUserProfilesTrim) {
-        #     $largeprofile = Get-ChildItem C:\Users\$userprofile -Recurse -Force -ErrorAction SilentlyContinue | Measure-Object -Sum length | Select-Object -ExpandProperty Sum
-        #     $largeprofile = [math]::Round($largeprofile / 1MB, 0)
-        #     $win32UserProfiles[$i].LocalProfileSize = $largeprofile
-        #     $win32UserProfiles[$i].ComputerName = $env:computername
-        #     $i++
-        # }
+    # #Uncomment to check profile sizes (this will add addtional time)
+    # #calculate estimated profile size
+    # $LocalUserProfiles = $Win32UserProfiles | Select-Object LocalPath
+    # $LocalUserProfilesTrim = ForEach ($LocalPath in $LocalUserProfiles) { $LocalPath.LocalPath.substring(9) }
+    # $i = 0
+    # foreach ($userprofile in $LocalUserProfilesTrim) {
+    #     $largeprofile = Get-ChildItem C:\Users\$userprofile -Recurse -Force -ErrorAction SilentlyContinue | Measure-Object -Sum length | Select-Object -ExpandProperty Sum
+    #     $largeprofile = [math]::Round($largeprofile / 1MB, 0)
+    #     $win32UserProfiles[$i].LocalProfileSize = $largeprofile
+    #     $win32UserProfiles[$i].ComputerName = $env:computername
+    #     $i++
+    # }
 
-        #return csv excluding headers & local accounts
-        $profiles = $Win32UserProfiles | Select-Object ComputerName, @{Name = "SelectedUserName"; EXPRESSION = { (New-Object System.Security.Principal.SecurityIdentifier($_.SID)).Translate([System.Security.Principal.NTAccount]).Value }; }, LocalPath , RoamingConfigured, Loaded, LocalProfileSize, @{Name = "JumpCloudUserName"; EXPRESSION = { ((New-Object System.Security.Principal.SecurityIdentifier($_.SID)).Translate([System.Security.Principal.NTAccount]).Value).Split('\')[1] }; }, TempPassword, AcceptEULA, LeaveDomain, ForceReboot, AzureADProfile, InstallJCAgent, JumpCloudConnectKey, Customxml, ConvertProfile, MigrationSuccess, DomainName
-        return ($profiles | ConvertTo-Csv -NoTypeInformation | Select-Object -Skip 1)
-        }
+    #return csv excluding headers & local accounts
+    $profiles = $Win32UserProfiles | Select-Object ComputerName, @{Name = "SelectedUserName"; EXPRESSION = { (New-Object System.Security.Principal.SecurityIdentifier($_.SID)).Translate([System.Security.Principal.NTAccount]).Value }; }, LocalPath , RoamingConfigured, Loaded, LocalProfileSize, @{Name = "JumpCloudUserName"; EXPRESSION = { ((New-Object System.Security.Principal.SecurityIdentifier($_.SID)).Translate([System.Security.Principal.NTAccount]).Value).Split('\')[1] }; }, TempPassword, AcceptEULA, LeaveDomain, ForceReboot, AzureADProfile, InstallJCAgent, JumpCloudConnectKey, Customxml, ConvertProfile, MigrationSuccess, DomainName
+    return ($profiles | ConvertTo-Csv -NoTypeInformation | Select-Object -Skip 1)
+}
 
 
 foreach ( $i in $OnlineComputers ) {
@@ -90,28 +94,31 @@ If (!(Test-Path $outputFolder)) {
 }
 
 #While $jobs = $true...
-While ($jobs) { #Begin $jobs While Loop
+While ($jobs) {
+    #Begin $jobs While Loop
 
     #Get all ADMU-DISCOVERY jobs
     $admujobs = (get-job) | where-object { $_.Name -eq 'ADMU-DISCOVERY' }
 
     #Use a ForEach loop to iterate through the jobs
-    foreach ($jobObject in $admujobs) { #Begin $ourJobs ForEach loop
-        
+    foreach ($jobObject in $admujobs) {
+        #Begin $ourJobs ForEach loop
+
         #Null out variables used in this loop cycle
-        $jobResults   = $null
+        $jobResults = $null
         $errorMessage = $null
-        $jobFile      = $null
-        $jobCommand   = $null
+        $jobFile = $null
+        $jobCommand = $null
 
         #Store the command used in the job to display later
-        $jobCommand   = $jobObject.Command
+        $jobCommand = $jobObject.Command
 
         #Use the Switch statement to take different actions based on the job's state value
-        Switch ($jobObject.State) { #Begin Job State Switch
+        Switch ($jobObject.State) {
+            #Begin Job State Switch
 
             #If the job state is running, display the job info
-            {$_ -eq 'Running'} {
+            { $_ -eq 'Running' } {
                 #Write-Host "Job: [$($jobObject.Name)] is still running on " $jobObject.Location
             }
 
@@ -119,8 +126,8 @@ While ($jobs) { #Begin $jobs While Loop
             #Then display different information if an error is found, versus successful completion
             #Use a here-string to create the file contents, then add the contents to the file
             #Finally use Remove-Job to remove the job
-            {$_ -eq 'Completed'} {
-                
+            { $_ -eq 'Completed' } {
+
                 #Create file
                 $jobFile = New-Item -Path $outputFolder -Name ("$($jobObject.Name)_$($jobObject.Location)_{0:MMddyy_HHmm}.txt" -f (Get-Date)) -ItemType File
 
@@ -154,7 +161,7 @@ $errorMessage
                     Add-Content -Path $jobFile -Value $fileContents
 
                 } else {
-                    
+
                     #Get job result and store in $jobResults
                     $jobResults = Receive-Job $jobObject.Name
 
@@ -167,7 +174,7 @@ Job Name: $($jobObject.Name)
 
 Job State: $($jobObject.State)
 
-Command: 
+Command:
 
 $jobCommand
 
@@ -190,23 +197,23 @@ $($jobResults | Out-String)
             #If the job state is failed, state that it is failed and then create the file
             #Add the error message to the file contents via a here-string
             #Then use Remove-Job to remove the job
-            {$_ -eq 'Failed'} {
+            { $_ -eq 'Failed' } {
 
                 #Create the file
-                $jobFile    = New-Item -Path $outputFolder -Name ("$($jobObject.Name)_$($jobObject.Location)_{0:MMddyy_HHmm}.txt" -f (Get-Date)) -ItemType File
+                $jobFile = New-Item -Path $outputFolder -Name ("$($jobObject.Name)_$($jobObject.Location)_{0:MMddyy_HHmm}.txt" -f (Get-Date)) -ItemType File
                 #Store the failure reason in $failReason
-                $failReason = $jobObject.ChildJobs[0].JobStateInfo.Reason.Message 
+                $failReason = $jobObject.ChildJobs[0].JobStateInfo.Reason.Message
 
                 Write-Host "Job: [$($jobObject.Name)] has failed on " $jobobject.Location`n
                 #Write-Host "$failReason"`n -ForegroundColor Red -BackgroundColor DarkBlue
-                
+
                 #Here-string that contains file contents
                 $fileContents = @"
 Job Name: $($jobObject.Name)
 
 Job State: $($jobObject.State)
 
-Command: 
+Command:
 
 $jobCommand
 
@@ -223,17 +230,17 @@ $failReason
 
 
         } #End Job State Switch
-     
+
     } #End $ourJobs ForEach loop
 
     #Clear the $ourJobs variable
     $ourJobs = $null
 
-    #Get the new list of ADMU-DISCOVERY jobs as it may have changed 
+    #Get the new list of ADMU-DISCOVERY jobs as it may have changed
     $ourJobs = (get-job) | where-object { $_.Name -eq 'ADMU-DISCOVERY' }
 
     #If jobs exists, keep the loop running by setting $jobs to $true, else set it to $false
-    if ($ourJobs) {$jobs = $true} else {$jobs = $false}
+    if ($ourJobs) { $jobs = $true } else { $jobs = $false }
 
     #Wait 10 seconds to check for jobs again
     Start-Sleep -Seconds $loopwait
--- jumpcloud-ADMU/Powershell/Private/SystemInfo/Get-NetBiosName.ps1
+++ jumpcloud-ADMU/Powershell/Private/SystemInfo/Get-NetBiosName.ps1
@@ -1,4 +1,8 @@
 function Get-NetBiosName {
-    $domain = (Get-WmiObject -Class Win32_ComputerSystem).Domain
+    try {
+        $domain = (Get-WmiObject -Class Win32_ComputerSystem).Domain
+    } catch {
+        $domain = (Get-CimInstance -Class Win32_ComputerSystem).Domain
+    }
     return $domain
 }
