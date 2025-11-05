e136d552978b5857205e30e8b4872785e7a57549
Ken Maranion <ken.maranion@jumpcloud.com>
2025-11-05 08:13:38 -0800

    added log

--- jumpcloud-ADMU/Powershell/Private/RegistryValidation/Backup-ProfileImageACL.ps1
+++ jumpcloud-ADMU/Powershell/Private/RegistryValidation/Backup-ProfileImageACL.ps1
@@ -10,30 +10,25 @@ function Backup-ProfileImageACL {
         $timestamp = Get-Date -Format "yyyyMMdd-HHmm"
         $path = "$ProfileImagePath\AppData\Local\JumpCloudADMU"
         $file = "$path\${sourceSID}_permission_backup_${timestamp}"
+        $ACLPermissionLogPath = "$(Get-WindowsDrive)\Windows\Temp\jcAdmu_ACL.log"
     }
     process {
-        try {
+        # Test if the directory exists. If not, create it recursively.
+        if (-Not (Test-Path -Path $path)) {
+            Write-ToLog "Creating directory: `"$path`"" -Level "Verbose"
+            New-Item -ItemType Directory -Force -Path $path | Out-Null
+        }
 
-            # Test if the directory exists. If not, create it recursively.
-            if (-Not (Test-Path -Path $path)) {
-                Write-ToLog "Creating directory: `"$path`"" -Level "Verbose"
-                New-Item -ItemType Directory -Force -Path $path | Out-Null
-            }
+        Write-ToLog "Backup file will be saved as: `"$file`"" -Level "Verbose"
+        Write-ToLog "Running ICACLS to save permissions. This may take a moment..." -Level "Verbose"
 
-            Write-ToLog "Backup file will be saved as: `"$file`"" -Level "Verbose"
-            Write-ToLog "Running ICACLS to save permissions. This may take a moment..." -Level "Verbose"
+        $aclResults = icacls $ProfileImagePath /save $file /T /C > $ACLPermissionLogPath 2>&1
 
-            $Output = icacls $ProfileImagePath /save $file /T /C /L 2>&1
-            $Summary = $Output | Select-Object -Last 1
-
-            Write-ToLog $Summary -Level "Verbose"
-
-            if ($LASTEXITCODE -eq 0) {
-                Write-ToLog "Permissions for '$ProfileImagePath' have been saved to '$file'." -Level "Verbose"
-            }
-        } catch {
-            # Catch errors from the ValidateScript block or other unexpected issues
-            Write-ToLog "An unexpected error occurred: $($_.Exception.Message)" -Level "Warning"
+        if ($LASTEXITCODE -ne 0) {
+            # Only log if there are non-filtered errors
+            Write-ToLog "Warning: icacls save operation had issues. Exit code: $LASTEXITCODE" -Level Verbose
+        } else {
+            Write-ToLog "Permissions for '$ProfileImagePath' have been saved to '$file'."
         }
     }
 }
