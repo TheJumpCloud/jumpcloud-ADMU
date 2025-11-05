f34bb5eb310bac1a93f372c8ce4178a572e85b84
Ken Maranion <ken.maranion@jumpcloud.com>
2025-11-04 16:10:53 -0800

    test

--- jumpcloud-ADMU/Powershell/Private/RegistryValidation/Backup-ProfileImageACL.ps1
+++ jumpcloud-ADMU/Powershell/Private/RegistryValidation/Backup-ProfileImageACL.ps1
@@ -6,43 +6,33 @@ function Backup-ProfileImageACL {
         [Parameter(Mandatory = $true, HelpMessage = "SID of the user.")]
         [string]$sourceSID
     )
-
-    try {
-        # No need to validate ProfileImagePath since it's validated prior to function execution
+    begin {
         $timestamp = Get-Date -Format "yyyyMMdd-HHmm"
-        $backupFileName = "${sourceSID}_permission_backup_${timestamp}"
+        $path = "$ProfileImagePath\AppData\Local\JumpCloudADMU"
+        $file = "$path\${sourceSID}_permission_backup_${timestamp}"
+    }
+    process {
+        try {
 
-        # Define the full directory path for the backup file
-        $backupDirectory = Join-Path -Path $ProfileImagePath -ChildPath "AppData\Local\JumpCloudADMU"
-
-        if (-not (Test-Path -Path $backupDirectory -PathType Container)) {
-            Write-ToLog "Creating backup directory: '$backupDirectory'"
-            try {
-                New-Item -Path $backupDirectory -ItemType Directory | Out-Null
-            } catch {
-                Write-ToLog "Failed to create backup directory: '$backupDirectory'. Error: $($_.Exception.Message)" -Level Error
-                # Prevent further execution if directory creation fails
-                return
+            # Test if the directory exists. If not, create it recursively.
+            if (!(Test-Path -Path $path -PathType Container)) {
+                New-Item -ItemType Directory -Force -Path $path | Out-Null
             }
 
+            Write-ToLog "Backup file will be saved as: `"$file`""
+            Write-ToLog "Running ICACLS to save permissions. This may take a moment..."
+
+            $Output = icacls $ProfileImagePath /save $file /T /C 2>&1
+            $Summary = $Output | Select-Object -Last 1
+
+            Write-ToLog $Summary
+
+            if ($LASTEXITCODE -eq 0) {
+                Write-ToLog "Permissions for '$ProfileImagePath' have been saved to '$file'."
+            }
+        } catch {
+            # Catch errors from the ValidateScript block or other unexpected issues
+            Write-ToLog "An unexpected error occurred: $($_.Exception.Message)" -Level Error
         }
-        # Define the full backup file path
-        $backupFilePath = Join-Path -Path $backupDirectory -ChildPath $backupFileName
-
-
-        Write-ToLog "Backup file will be saved as: `"$backupFilePath`""
-        Write-ToLog "Running ICACLS to save permissions. This may take a moment..."
-
-        $Output = icacls "$ProfileImagePath" /save "$backupFilePath" /T /C 2>&1
-        $Summary = $Output | Select-Object -Last 1
-
-        Write-ToLog $Summary
-
-        if ($LASTEXITCODE -eq 0) {
-            Write-ToLog "Permissions for '$ProfileImagePath' have been saved to '$backupFilePath'."
-        }
-    } catch {
-        # Catch errors from the ValidateScript block or other unexpected issues
-        Write-ToLog "An unexpected error occurred: $($_.Exception.Message)" -Level Error
     }
 }
